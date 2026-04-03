a55bc7724240b68ffb1bd172d13082de52e7288a
Jeremias Sosa Paz <jeremiasosapaz@gmail.com>
2026-04-03 10:35:25 -0300

    Implemento ParseArgs y ShowHelp

--- practicos/63546 - Sosa Paz, Jeremkas/TP1/sortx.cs
+++ practicos/63546 - Sosa Paz, Jeremkas/TP1/sortx.cs
@@ -39,3 +39,47 @@ record AppConfig(
     List<SortField> SortFields,
     bool ShowHelp
 );
+// --- 3. FUNCIONES LOCALES ---
+
+AppConfig ParseArgs(string[] args)
+{
+    string? input = null;
+    string? output = null;
+    string delimiter = ",";
+    bool noHeader = false;
+    bool showHelp = false;
+    var sortFields = new List<SortField>();
+
+    int positionalCount = 0;
+
+    for (int i = 0; i < args.Length; i++)
+    {
+        string arg = args[i];
+        if (arg == "-h" || arg == "--help") showHelp = true;
+        else if (arg == "-nh" || arg == "--no-header") noHeader = true;
+        else if (arg == "-d" || arg == "--delimiter") delimiter = args[++i].Replace("\\t", "\t");
+        else if (arg == "-i" || arg == "--input") input = args[++i];
+        else if (arg == "-o" || arg == "--output") output = args[++i];
+        else if (arg == "-b" || arg == "--by")
+        {
+            var parts = args[++i].Split(':');
+            string name = parts[0];
+            bool isNum = parts.Length > 1 && parts[1] == "num";
+            bool isDesc = parts.Length > 2 && parts[2] == "desc";
+            sortFields.Add(new SortField(name, isNum, isDesc));
+        }
+        else
+        {
+            if (positionalCount == 0) input = arg;
+            else if (positionalCount == 1) output = arg;
+            positionalCount++;
+        }
+    }
+
+    return new AppConfig(input, output, delimiter, noHeader, sortFields, showHelp);
+}
+
+void ShowHelp()
+{
+    Console.WriteLine("Uso: sortx [input [output]] [-b|--by campo[:tipo[:orden]]]... [-d delimitador] [-nh] [-h]");
+}
