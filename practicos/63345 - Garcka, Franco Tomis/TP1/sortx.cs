e3557441274f9a9277aa93eff30584673a260356
Franco Tomas Garcia <143847992+FrancoTms@users.noreply.github.com>
2026-04-06 17:34:48 -0300

    Agrego records

--- practicos/63345 - Garcka, Franco Tomis/TP1/sortx.cs
+++ practicos/63345 - Garcka, Franco Tomis/TP1/sortx.cs
@@ -5,3 +5,12 @@
 //       [-nh|--no-header] [-h|--help]
 
 Console.WriteLine("Inicio sortx");
+record SortField(string Name, bool Numeric, bool Descending);
+
+record AppConfig(
+    string? InputFile,
+    string? OutputFile,
+    string Delimiter,
+    bool NoHeader,
+    List<SortField> SortFields
+);
