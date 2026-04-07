180701d5abf8c3b88f756794cf42ff97884c9751
Franco Tomas Garcia <143847992+FrancoTms@users.noreply.github.com>
2026-04-07 19:00:16 -0300

    Agrego Opciones y ShowHelp

--- practicos/63345 - Garcka, Franco Tomis/TP1/sortx.cs
+++ practicos/63345 - Garcka, Franco Tomis/TP1/sortx.cs
@@ -24,32 +24,80 @@ AppConfig ParseArgs(string[] args)
     string delimiter = ",";
     bool noHeader = false;
     var sortFields = new List<SortField>();
+
     int i = 0;
     if (i < args.Length && !args[i].StartsWith("-"))
     {
         input = args[i++];
     }
-
     if (i < args.Length && !args[i].StartsWith("-"))
     {
         output = args[i++];
     }
-
     while (i < args.Length)
     {
         var arg = args[i];
-        if (arg == "-b" || arg == "--by")
+        switch (arg)
         {
-            var value = args[++i];
-            var parts = value.Split(':');
-            string name = parts[0];
-            bool numeric = parts.Length > 1 && parts[1] == "num";
-            bool descending = parts.Length > 2 && parts[2] == "desc";
-            sortFields.Add(new SortField(name, numeric, descending));
+            case "-i":
+            case "--input":
+                input = args[++i];
+                break;
+
+            case "-o":
+            case "--output":
+                output = args[++i];
+                break;
+
+            case "-d":
+            case "--delimiter":
+                var d = args[++i];
+                delimiter = d == "\\t" ? "\t" : d;
+                break;
+
+            case "-nh":
+            case "--no-header":
+                noHeader = true;
+                break;
+
+            case "-b":
+            case "--by":
+                var value = args[++i];
+                var parts = value.Split(':');
+
+                string name = parts[0];
+                bool numeric = parts.Length > 1 && parts[1] == "num";
+                bool descending = parts.Length > 2 && parts[2] == "desc";
+
+                sortFields.Add(new SortField(name, numeric, descending));
+                break;
+
+            case "-h":
+            case "--help":
+                ShowHelp();
+                Environment.Exit(0);
+                break;
+
+            default:
+                throw new Exception($"Argumento desconocido: {arg}");
         }
         i++;
     }
     return new AppConfig(input, output, delimiter, noHeader, sortFields);
+}
+//Show Help
+void ShowHelp()
+{
+    Console.WriteLine("Uso:");
+    Console.WriteLine("sortx [input [output]] [opciones]");
+    Console.WriteLine("");
+    Console.WriteLine("Opciones:");
+    Console.WriteLine("-b, --by campo[:tipo[:orden]]");
+    Console.WriteLine("-i, --input archivo");
+    Console.WriteLine("-o, --output archivo");
+    Console.WriteLine("-d, --delimiter delimitador");
+    Console.WriteLine("-nh, --no-header");
+    Console.WriteLine("-h, --help");
 }
 //ReadInput
 string ReadInput(AppConfig config)
@@ -186,3 +234,4 @@ record AppConfig(
     bool NoHeader,
     List<SortField> SortFields
 );
+
