1f734413d603f089410daef7a73e9d2b700fd65c
Ignacio Gonzalez Dupuy <nachogonzadup@gmail.com>
2026-04-08 16:25:11 -0300

    segundo commit

--- practicos/63457 - Gonzalez Dupuy, Ignacio/TP1/sortx.cs
+++ practicos/63457 - Gonzalez Dupuy, Ignacio/TP1/sortx.cs
@@ -3,17 +3,35 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 
+Opciones LeerArgumentos(string[] args)
+{
+    string? entrada = null;
+    string? salida = null;
+    var criterios = new List<Criterio>();
+    for (int i = 0; i < args.Length; i++)
+    {
+        if (args[i] == "-b")
+        {
+            var col = args[++i];
+            criterios.Add(new Criterio(col, false, false));
+        }
+        else if (entrada == null)
+            entrada = args[i];
+        else if (salida == null)
+            salida = args[i];
+    }
+    return new Opciones(entrada, salida, ",", false, criterios);
+}
 try
 {
-    Console.WriteLine("Inicio del programa");
+    var opciones = LeerArgumentos(args);
+    Console.WriteLine(opciones.Entrada);
 }
 catch (Exception ex)
 {
     Console.Error.WriteLine(ex.Message);
 }
-
 record Criterio(string Columna, bool EsNumero, bool EsDesc);
-
 record Opciones(
     string? Entrada,
     string? Salida,
