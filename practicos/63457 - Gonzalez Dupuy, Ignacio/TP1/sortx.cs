7066542a9117bd207bf084a994754abb226ce355
Ignacio Gonzalez Dupuy <nachogonzadup@gmail.com>
2026-04-08 18:28:03 -0300

    4to commit

--- practicos/63457 - Gonzalez Dupuy, Ignacio/TP1/sortx.cs
+++ practicos/63457 - Gonzalez Dupuy, Ignacio/TP1/sortx.cs
@@ -24,16 +24,35 @@ Opciones LeerArgumentos(string[] args)
 }
 string LeerTexto(Opciones op)
 {
-    if (op.Entrada != null)
-        return File.ReadAllText(op.Entrada);
-    else
-        return Console.In.ReadToEnd();
+    return op.Entrada != null
+        ? File.ReadAllText(op.Entrada)
+        : Console.In.ReadToEnd();
+}
+List<string[]> SepararFilas(string texto, Opciones op)
+{
+    return texto
+        .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+        .Select(l => l.Split(op.Separador))
+        .ToList();
+}
+List<string[]> Ordenar(List<string[]> filas, Opciones op)
+{
+    var cabecera = filas[0];
+    var datos = filas.Skip(1).ToList();
+    int col = Array.IndexOf(cabecera, op.Criterios[0].Columna);
+    var ordenadas = datos.OrderBy(f => f[col]).ToList();
+    var resultado = new List<string[]> { cabecera };
+    resultado.AddRange(ordenadas);
+    return resultado;
 }
 try
 {
     var opciones = LeerArgumentos(args);
     var texto = LeerTexto(opciones);
-    Console.WriteLine(texto);
+    var filas = SepararFilas(texto, opciones);
+    var ordenadas = Ordenar(filas, opciones);
+    foreach (var fila in ordenadas)
+        Console.WriteLine(string.Join(",", fila));
 }
 catch (Exception ex)
 {
