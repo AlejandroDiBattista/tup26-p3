fa85d7d698526dc67f0711b86470170019d6a7a0
juancarrer11 <juancarrer11@gmail.com>
2026-04-15 21:20:25 -0300

    Agregar archivos iniciales para el proyecto TP2, incluyendo la clase Program y el archivo de proyecto. Tambien agrego help.

---
+++ practicos/63341 - Carrer, Juan Cruz/TP2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] argumentos)
+    {
+        if (argumentos.Length > 0)
+        {
+            if (argumentos.Length > 0 && (argumentos[0] == "--help" || argumentos[0] == "-h"))
+            {
+                Console.WriteLine("Operaciones disponibles:");
+                Console.WriteLine("Suma: +");
+                Console.WriteLine("Resta: -");
+                Console.WriteLine("Multiplicación: *");
+                Console.WriteLine("División entera: /");
+                Console.WriteLine("Módulo: %");
+                Console.WriteLine("Paréntesis: ()");
+                Console.WriteLine();
+                Console.WriteLine("Modo interactivo:");
+                Console.WriteLine("Ingresar las operaciones manualmente");
+                Console.WriteLine("Escriba \"salir\" para terminar");
+                return;
+            }
+
+            
