cbdd3bcf06aa3a33c322cc38b6f3f5f5f4d57580
juancarrer11 <juancarrer11@gmail.com>
2026-04-15 22:13:26 -0300

    Agregar método GenerarTokens para tokenizar expresiones matemáticas en la clase Calculadora

--- practicos/63341 - Carrer, Juan Cruz/TP2/calculadora.cs
+++ practicos/63341 - Carrer, Juan Cruz/TP2/calculadora.cs
@@ -9,3 +9,43 @@ class Calculadora
         var expresionRPN = ConvertirAPostfijo(listaTokens);
         return CalcularRPN(expresionRPN);
     }
+    private static List<ElementoToken> GenerarTokens(string textoEntrada)
+    {
+        List<ElementoToken> listaTokens = new List<ElementoToken>();
+
+        for (int indice = 0; indice < textoEntrada.Length; indice++)
+        {
+            char caracterActual = textoEntrada[indice];
+
+            if (char.IsWhiteSpace(caracterActual)) continue;
+
+            if (char.IsDigit(caracterActual))
+            {
+                string numeroConstruido = "";
+
+                while (indice < textoEntrada.Length && char.IsDigit(textoEntrada[indice]))
+                {
+                    numeroConstruido += textoEntrada[indice];
+                    indice++;
+                }
+
+                indice--;
+                listaTokens.Add(new ElementoToken(TipoToken.Numero, numeroConstruido));
+            }
+            else
+            {
+                switch (caracterActual)
+                {
+                    case '+': listaTokens.Add(new ElementoToken(TipoToken.OperadorSuma)); break;
+                    case '-': listaTokens.Add(new ElementoToken(TipoToken.OperadorResta)); break;
+                    case '*': listaTokens.Add(new ElementoToken(TipoToken.OperadorMultiplicacion)); break;
+                    case '/': listaTokens.Add(new ElementoToken(TipoToken.OperadorDivision)); break;
+                    case '%': listaTokens.Add(new ElementoToken(TipoToken.OperadorModulo)); break;
+                    case '(': listaTokens.Add(new ElementoToken(TipoToken.ParentesisApertura)); break;
+                    case ')': listaTokens.Add(new ElementoToken(TipoToken.ParentesisCierre)); break;
+                }
+            }
+        }
+
+        return listaTokens;
+    }
