12bfc0a362d146807c0c83848d0092ccb198419f
Franco Tomas Garcia <143847992+FrancoTms@users.noreply.github.com>
2026-04-16 10:48:54 -0300

    Agrego operaciones de suma y resta

--- practicos/63345 - García, Franco Tomás/TP2/integer.cs
+++ practicos/63345 - García, Franco Tomás/TP2/integer.cs
@@ -83,6 +83,91 @@ using System.Text;
 
     public bool EsCero() => listaDigitos.Count == 1 && listaDigitos[0] == 0;
 
+    public static Integer operator +(Integer primero, Integer segundo)
+    {
+        if (primero.esNegativo == segundo.esNegativo)
+            return new Integer(SumarListas(primero.listaDigitos, segundo.listaDigitos), primero.esNegativo);
+
+        if (CompararAbsolutos(primero, segundo) >= 0)
+            return new Integer(RestarListas(primero.listaDigitos, segundo.listaDigitos), primero.esNegativo);
+
+        return new Integer(RestarListas(segundo.listaDigitos, primero.listaDigitos), segundo.esNegativo);
+    }
+
+    public static Integer operator -(Integer primero, Integer segundo) => primero + (-segundo);
+
+    public static Integer operator -(Integer numero)
+        => new Integer(new List<int>(numero.listaDigitos), !numero.esNegativo);
+
+    public static Integer operator *(Integer primero, Integer segundo)
+    {
+        var resultado = new int[primero.listaDigitos.Count + segundo.listaDigitos.Count];
+
+        for (int i = 0; i < primero.listaDigitos.Count; i++)
+        {
+            for (int j = 0; j < segundo.listaDigitos.Count; j++)
+            {
+                resultado[i + j] += primero.listaDigitos[i] * segundo.listaDigitos[j];
+                resultado[i + j + 1] += resultado[i + j] / 10;
+                resultado[i + j] %= 10;
+            }
+        }
+
+        return new Integer(new List<int>(resultado), primero.esNegativo ^ segundo.esNegativo);
+    }
+
+    private static List<int> SumarListas(List<int> listaA, List<int> listaB)
+    {
+        List<int> resultado = new List<int>();
+        int acarreo = 0;
+
+        for (int i = 0; i < Math.Max(listaA.Count, listaB.Count) || acarreo > 0; i++)
+        {
+            int suma = acarreo;
+            if (i < listaA.Count) suma += listaA[i];
+            if (i < listaB.Count) suma += listaB[i];
+
+            resultado.Add(suma % 10);
+            acarreo = suma / 10;
+        }
+
+        return resultado;
+    }
+
+    private static List<int> RestarListas(List<int> listaA, List<int> listaB)
+    {
+        List<int> resultado = new List<int>();
+        int prestamo = 0;
+
+        for (int i = 0; i < listaA.Count; i++)
+        {
+            int diferencia = listaA[i] - prestamo - (i < listaB.Count ? listaB[i] : 0);
+
+            if (diferencia < 0)
+            {
+                diferencia += 10;
+                prestamo = 1;
+            }
+            else prestamo = 0;
+
+            resultado.Add(diferencia);
+        }
+
+        return resultado;
+    }
+
+    private static int CompararAbsolutos(Integer primero, Integer segundo)
+    {
+        if (primero.listaDigitos.Count != segundo.listaDigitos.Count)
+            return primero.listaDigitos.Count.CompareTo(segundo.listaDigitos.Count);
+
+        for (int i = primero.listaDigitos.Count - 1; i >= 0; i--)
+            if (primero.listaDigitos[i] != segundo.listaDigitos[i])
+                return primero.listaDigitos[i].CompareTo(segundo.listaDigitos[i]);
+
+        return 0;
+    }
+
     public override string ToString()
     {
         StringBuilder constructorTexto = new StringBuilder();
