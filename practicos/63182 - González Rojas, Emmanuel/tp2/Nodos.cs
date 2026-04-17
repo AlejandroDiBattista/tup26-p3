fd437a609544be4a4872802edf3bacf3df8342fd
EmmanuelGR-web <emmanuelgonzalez08.eg@gmail.com>
2026-04-16 23:38:25 -0300

    Agrego la clase VariableNodo para representar la variable 'x' y tambien la clase NodoUnario que sera base para binarios y sus derivados 'PositivoNodo' y 'NegativoNodo'

--- practicos/63182 - González Rojas, Emmanuel/tp2/Nodos.cs
+++ practicos/63182 - González Rojas, Emmanuel/tp2/Nodos.cs
@@ -1,7 +1,7 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
-
+//-----------------------------------
 class NumeroNodo : Nodo {
     public int Valor { get; }
 
@@ -11,3 +11,26 @@ class NumeroNodo : Nodo {
 
     public override int Evaluar(int x = 0) => Valor;
 }
+//-----------------------------------
+class VariableNodo : Nodo {
+    public override int Evaluar(int x = 0) => x;
+}
+abstract class NodoUnario : Nodo {
+    protected Nodo Operando;
+
+    public NodoUnario(Nodo operando) {
+        Operando = operando;
+    }
+}
+
+class PositivoNodo : NodoUnario {
+    public PositivoNodo(Nodo operando) : base(operando) { }
+    public override int Evaluar(int x = 0) => Operando.Evaluar(x);
+}
+
+
+class NegativoNodo : NodoUnario {
+    public NegativoNodo(Nodo operando) : base(operando) { }
+    public override int Evaluar(int x = 0) => -Operando.Evaluar(x);
+}
+
