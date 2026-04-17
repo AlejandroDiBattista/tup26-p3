e1315c71ce14969d5c3352f96fad7177ef631315
EmmanuelGR-web <emmanuelgonzalez08.eg@gmail.com>
2026-04-16 23:26:02 -0300

    agrego la clase NumeroNodo para representar literales enteros

--- practicos/63182 - González Rojas, Emmanuel/tp2/Nodos.cs
+++ practicos/63182 - González Rojas, Emmanuel/tp2/Nodos.cs
@@ -1,3 +1,13 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
+
+class NumeroNodo : Nodo {
+    public int Valor { get; }
+
+    public NumeroNodo(int valor) {
+        Valor = valor;
+    }
+
+    public override int Evaluar(int x = 0) => Valor;
+}
