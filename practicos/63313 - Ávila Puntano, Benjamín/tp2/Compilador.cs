ab469f3a217939a613eb16ed262233e7edc22344
bnjavila07 <bnjavilapuntano@gmail.com>
2026-04-20 10:02:17 -0300

    Inicio del tp2, agrego variables en el cs del compilador

--- practicos/63313 - Ávila Puntano, Benjamín/tp2/Compilador.cs
+++ practicos/63313 - Ávila Puntano, Benjamín/tp2/Compilador.cs
@@ -1,5 +1,15 @@
 class Compilador {
     public static Nodo Parse(string expresion) {
+
+        if (string.IsNullOrEmpty(expresion))
+        {
+            throw new ArgumentException("La expresión no puede estar vacía.");
+        }
+
+        var recorrer = new recorrer(expresion);
+        var raiz = recorrer.ParsearExpresion();
+
+        
         throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
     }
 }
