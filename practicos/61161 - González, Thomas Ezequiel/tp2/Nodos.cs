027a261aff965376e73bd4a4aa3f3783110d7c2b
Kobinee <thomasg25610@gmail.com>
2026-04-20 21:16:09 -0300

    agregue los nodos base: NumeroNodo y VariableNodo con pruebas basicas

--- practicos/61161 - González, Thomas Ezequiel/tp2/Nodos.cs
+++ practicos/61161 - González, Thomas Ezequiel/tp2/Nodos.cs
@@ -1,3 +1,25 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
+
+class NumeroNodo : Nodo
+{
+    private int valor;
+
+    public NumeroNodo(int valor)
+    {
+        this.valor = valor;
+    }
+
+    public override int Evaluar(int x = 0)
+    {
+        return valor;
+    }
+}
+class VariableNodo : Nodo
+{
+    public override int Evaluar(int x = 0)
+    {
+        return x;
+    }
+}
