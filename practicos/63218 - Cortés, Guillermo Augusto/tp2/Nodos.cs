1ab2973f76b5a1ced13a84d7e8315c77d38441bc
GuilleCortes01 <guillecortes100@gmail.com>
2026-04-21 18:28:10 -0300

    Agregue clases para operaciones binarias: Suma, Resta, Multiplicacion y Division

--- practicos/63218 - Cortés, Guillermo Augusto/tp2/Nodos.cs
+++ practicos/63218 - Cortés, Guillermo Augusto/tp2/Nodos.cs
@@ -20,3 +20,43 @@ class Negativo : Nodo
     public Negativo(Nodo nodo) => this.nodo = nodo;
     public override int Evaluar(int x = 0) => -nodo.Evaluar(x);
 }
+
+abstract class Binario : Nodo
+{
+    protected Nodo izq, der;
+    public Binario(Nodo izq, Nodo der)
+    {
+        this.izq = izq;
+        this.der = der;
+    }
+}
+
+class Suma : Binario
+{
+    public Suma(Nodo i, Nodo d) : base(i, d) { }
+    public override int Evaluar(int x = 0) => izq.Evaluar(x) + der.Evaluar(x);
+}
+
+class Resta : Binario
+{
+    public Resta(Nodo i, Nodo d) : base(i, d) { }
+    public override int Evaluar(int x = 0) => izq.Evaluar(x) - der.Evaluar(x);
+}
+
+class Multiplicacion : Binario
+{
+    public Multiplicacion(Nodo i, Nodo d) : base(i, d) { }
+    public override int Evaluar(int x = 0) => izq.Evaluar(x) * der.Evaluar(x);
+}
+
+class Division : Binario
+{
+    public Division(Nodo i, Nodo d) : base(i, d) { }
+
+    public override int Evaluar(int x = 0)
+    {
+        var division = der.Evaluar(x);
+        if (division == 0) throw new DivideByZeroException();
+        return izq.Evaluar(x) / division;
+    }
+}
