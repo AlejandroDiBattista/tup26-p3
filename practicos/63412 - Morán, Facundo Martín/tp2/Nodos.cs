c118bc4b3ee6b899c5388a65c03f6f3e48aef5cb
Facundo Morán <facumamoran@gmail.com>
2026-04-21 01:07:05 -0300

    Agrego nodos hoja (NumeroNodo, VariableNodo y NegativoNodo)

--- practicos/63412 - Morán, Facundo Martín/tp2/Nodos.cs
+++ practicos/63412 - Morán, Facundo Martín/tp2/Nodos.cs
@@ -1,3 +1,12 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
+class NumeroNodo(int valor) : Nodo {
+    public override int Evaluar(int x = 0) => valor;
+    }
+class VariableNodo : Nodo {
+    public override int Evaluar(int x = 0) => x;
+}
+class NegativoNodo(Nodo operando) : Nodo {
+    public override int Evaluar(int x = 0) => -operando.Evaluar(x);
+}
