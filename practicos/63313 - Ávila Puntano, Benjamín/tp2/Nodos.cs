3db840fe37c6af3ab07ddeef457418f69593e353
bnjavila07 <bnjavilapuntano@gmail.com>
2026-04-21 18:58:07 -0300

    agregado de clase resta con su funcion

--- practicos/63313 - Ávila Puntano, Benjamín/tp2/Nodos.cs
+++ practicos/63313 - Ávila Puntano, Benjamín/tp2/Nodos.cs
@@ -3,7 +3,7 @@ abstract class Nodo {
 }
 
 class numero (int valor) : Nodo{
-    public override int evaluar ( int x = 0) => valor;
+    public override int evaluar ( int x = 0) => valor; 
 }
 
 class variable : Nodo
@@ -13,11 +13,16 @@ class variable : Nodo
 
 abstract class binario(Nodo izquierdo, Nodo derecha) : Nodo
 {
-    protected readonly Nodo izquierdo = izquierdo;
+    protected readonly Nodo izquierdo = izquierdo; 
     protected readonly Nodo derecha = derecha;
 }
 
 class Suma(Nodo izquierdo, Nodo derecho) : binario(izquierdo,derecha)
 {
     public override int evaluar(int x=0) => izquierdo.Evaluar(x) + derecha.Evaluar(x);
+}
+
+class resta(Nodo izquierdo, Nodo derecho) : binario(izquierdo, derecha)
+{
+    public override int evaluar (int x=0) => izquierdo.Evaluar(x) - derecha.Evaluar(x);
 }
