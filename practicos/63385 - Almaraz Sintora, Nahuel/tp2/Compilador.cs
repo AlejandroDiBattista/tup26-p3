db7a456b0fb2672e453c68d5b81ccd91762f43b9
Nahuel Almaraz Sintora <nahuelalmaraz957@gmail.com>
2026-04-21 20:09:54 -0300

    Agrego parser basico de expresiones en el compilador

--- practicos/63385 - Almaraz Sintora, Nahuel/tp2/Compilador.cs
+++ practicos/63385 - Almaraz Sintora, Nahuel/tp2/Compilador.cs
@@ -1,5 +1,13 @@
 class Compilador {
+    private string _expresion = "";
+    private int _pos = 0;
     public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+        var compilador = new Compilador(expresion);
+        var nodo = compilador.ParseExpresion();
+        compilador.SaltarEspacios();
+        if (compilador._pos < compilador._expresion.Length) {
+            throw new FormatException($"Token inesperado: '{compilador._expresion[compilador._pos]}'");
+        }
+        return nodo;
     }
 }
