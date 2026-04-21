067688c54d1f259dff59d7bd684d21f809ecc32b
julianbajre-png <julianbajre@gmail.com>
2026-04-21 18:32:55 -0300

    Parser Basico
    implementacion del archivo compilador con un parser de descenso recursivo que procesa numeros enteros de multiples digitos, la variable x y los cuatro operadores

--- practicos/63222 - Bajre Martínez, Julián/tp2/Compilador.cs
+++ practicos/63222 - Bajre Martínez, Julián/tp2/Compilador.cs
@@ -1,5 +1,74 @@
-class Compilador {
-    public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+using System;
+using System.Collections.Generic;
+namespace TP2.Calculadora;
+
+public class Compilador
+{
+    private string _expresion = "";
+    private int _posicion = 0;
+
+    public Nodo Parsear(string expresion)
+    {
+        _expresion = expresion.Replace(" ", "");
+        _posicion = 0;
+        return ParsearExpresion();
+    }
+
+    private char CharActual => _posicion < _expresion.Length ? _expresion[_posicion] : '\0';
+
+    private char Consumir() => _expresion[_posicion++];
+
+    private Nodo ParsearExpresion()
+    {
+        Nodo izq = ParsearTermino();
+
+        while (CharActual == '+' || CharActual == '-')
+        {
+            char operador = Consumir();
+            Nodo der = ParsearTermino();
+            izq = operador == '+' ? new NodoSuma(izq, der) : new NodoResta(izq, der);
+        }
+
+        return izq;
+    }
+
+    private Nodo ParsearTermino()
+    {
+        Nodo izq = ParsearFactor();
+
+        while (CharActual == '*' || CharActual == '/')
+        {
+            char operador = Consumir();
+            Nodo der = ParsearFactor();
+            izq = operador == '*' ? new NodoProducto(izq, der) : new NodoCociente(izq, der);
+        }
+
+        return izq;
+    }
+
+    private Nodo ParsearFactor()
+    {
+        if (CharActual == 'x' || CharActual == 'X')
+        {
+            Consumir();
+            return new NodoVariable();
+        }
+
+        if (char.IsDigit(CharActual))
+        {
+            return LeerNumero();
+        }
+
+        throw new Exception($"Token inesperado '{CharActual}' en posición {_posicion}.");
+    }
+
+    private NodoNumero LeerNumero()
+    {
+        int inicio = _posicion;
+        while (char.IsDigit(CharActual))
+            Consumir();
+
+        int valor = int.Parse(_expresion.Substring(inicio, _posicion - inicio));
+        return new NodoNumero(valor);
     }
 }
