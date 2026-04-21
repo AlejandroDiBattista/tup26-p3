d0ec7c97250944407e7a889ccac383e5b5cfe5c5
Kobinee <thomasg25610@gmail.com>
2026-04-20 21:42:13 -0300

    agregue nodos para operaciones binarias tambien con la suma.
    Hice pruebas basicas de la suma y todas funcionaron

--- practicos/61161 - González, Thomas Ezequiel/tp2/Nodos.cs
+++ practicos/61161 - González, Thomas Ezequiel/tp2/Nodos.cs
@@ -1,7 +1,7 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
-
+// Nodo para números constantes
 class NumeroNodo : Nodo
 {
     private int valor;
@@ -16,10 +16,49 @@ class NumeroNodo : Nodo
         return valor;
     }
 }
+// Nodo para la variable 'x'
 class VariableNodo : Nodo
 {
     public override int Evaluar(int x = 0)
     {
         return x;
+    }
+}
+// Nodo para la negación unaria 
+class NegativoNodo : Nodo
+{
+    private Nodo nodo;
+
+    public NegativoNodo(Nodo nodo)
+    {
+        this.nodo = nodo;
+    }
+
+    public override int Evaluar(int x = 0)
+    {
+        return -nodo.Evaluar(x);
+    }
+}
+
+// Nodo para operaciones binarias
+abstract class NodoBinario : Nodo
+{
+    protected Nodo izquierda;
+    protected Nodo derecha;
+
+    public NodoBinario(Nodo izquierda, Nodo derecha)
+    {
+        this.izquierda = izquierda;
+        this.derecha = derecha;
+    }
+}
+// Nodo para la suma
+class SumaNodo : NodoBinario
+{
+    public SumaNodo(Nodo izq, Nodo der) : base(izq, der) {}
+
+    public override int Evaluar(int x = 0)
+    {
+        return izquierda.Evaluar(x) + derecha.Evaluar(x);
     }
 }
