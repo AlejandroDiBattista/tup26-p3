c11384026308c03473e2b35680e09b66953401b0
Juan Cruz Decima <devjuancruz003@gmail.com>
2026-04-21 20:37:07 -0300

    agrego clase  NumeroNodo

--- practicos/63266 - Decima, Juan Cruz/tp2/Nodos.cs
+++ practicos/63266 - Decima, Juan Cruz/tp2/Nodos.cs
@@ -1,3 +1,23 @@
-abstract class Nodo {
+abstract class Nodo
+{
     public abstract int Evaluar(int x = 0);
 }
+
+class NumeroNodo : Nodo
+{
+
+    private readonly int valor;
+
+    public NumeroNodo(int valor)
+    {
+        this.valor = valor;
+    }
+
+    public override int Evaluar(int x = 0)
+    {
+        return valor;
+    }
+
+
+}
+
