764bc5acc1605b7ca256bef3bdcc4e8a615dc56f
Kobinee <thomasg25610@gmail.com>
2026-04-21 20:50:13 -0300

    Agregue ParseExpresion para manejar suma y resta

--- practicos/61161 - González, Thomas Ezequiel/tp2/Compilador.cs
+++ practicos/61161 - González, Thomas Ezequiel/tp2/Compilador.cs
@@ -1,5 +1,49 @@
-class Compilador {
-    public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+class Compilador
+{
+    private string texto;
+    private int posicion;
+
+    private Compilador(string texto)
+    {
+        this.texto = texto;
+        this.posicion = 0;
+    }
+
+    public static Nodo Parse(string expresion)
+    {
+        var compilador = new Compilador(expresion);
+        return compilador.ParsearNumero();
+    }
+
+    private char Actual()
+    {
+        if (posicion >= texto.Length)
+            return '\0';
+
+        return texto[posicion];
+    }
+
+    private void Avanzar()
+    {
+        posicion++;
+    }
+
+    private Nodo ParsearNumero()
+    {
+        int inicio = posicion;
+
+        while (char.IsDigit(Actual()))
+        {
+            Avanzar();
+        }
+
+        string numeroTexto = texto.Substring(inicio, posicion - inicio);
+
+        if (numeroTexto.Length == 0)
+            throw new Exception("Se esperaba un número");
+
+        int valor = int.Parse(numeroTexto);
+
+        return new NumeroNodo(valor);
     }
 }
