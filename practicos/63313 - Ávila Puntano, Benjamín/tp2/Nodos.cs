e15daf843afc9b37d92cf257558c767adbf8a65c
bnjavila07 <bnjavilapuntano@gmail.com>
2026-04-21 20:36:48 -0300

    agregado de multiplicacion y division

--- practicos/63313 - Ávila Puntano, Benjamín/tp2/Nodos.cs
+++ practicos/63313 - Ávila Puntano, Benjamín/tp2/Nodos.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
@@ -25,4 +27,25 @@ class Suma(Nodo izquierdo, Nodo derecho) : binario(izquierdo,derecha)
 class resta(Nodo izquierdo, Nodo derecho) : binario(izquierdo, derecha)
 {
     public override int evaluar (int x=0) => izquierdo.Evaluar(x) - derecha.Evaluar(x);
+
 }
+
+class multiplicacionproduc(Nodo izquierdo, Nodo derecho) : binario(izquierdo, derecha)
+{
+    public override int Evaluar (int x=0) => izquierdo.Evaluar(x) * derecha.Evaluar(x);
+
+}
+
+class division(Nodo izquierdo, Nodo derecho) : binario(izquierdo, derecha)
+{
+    public override int Evaluar(int x = 0)
+    {
+            variable divisor = derecha.Evaluar(x = 0);
+
+            if (divisor => 0) 
+            throw new DivideByZeroException ("es division por cero");
+            return izquierda.evaluar(x) / divisor;
+    }
+
+    
+    }
