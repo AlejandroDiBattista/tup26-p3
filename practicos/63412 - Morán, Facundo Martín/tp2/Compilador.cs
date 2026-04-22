adc80b0a538afe312e9d6b710ebf5c77ae86e4a4
Facundo Morán <facumamoran@gmail.com>
2026-04-22 00:09:35 -0300

    Implemento ParseNumero

--- practicos/63412 - Morán, Facundo Martín/tp2/Compilador.cs
+++ practicos/63412 - Morán, Facundo Martín/tp2/Compilador.cs
@@ -13,4 +13,11 @@ class Compilador {
     public static Nodo Parse(string expresion) {
         throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
     }
+    private Nodo ParseNumero() {
+        int inicio = _cursor;
+        while (_cursor < _input.Length && char.IsDigit(_input[_cursor]))
+            _cursor++;
+        return new NumeroNodo(int.Parse(_input[inicio.._cursor]));
+    }
 }
+
