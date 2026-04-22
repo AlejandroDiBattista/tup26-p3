9c34f4493061b222c4ce9330c7c02f9717debd21
hernancarpalvarez28 <hernancarpalvarez@gmail.com>
2026-04-22 20:24:39 -0300

    Agrego ParsearFactor para convertir un numero a numNodo, si es x a varNodo y si es parentesis usa parsearExpresion para resolver lo que esta en el parentesis

--- practicos/63300 - Álvarez, Hernán Fabricio/tp2/Compilador.cs
+++ practicos/63300 - Álvarez, Hernán Fabricio/tp2/Compilador.cs
@@ -14,7 +14,7 @@ public class Compilador {
          _texto = texto;
          _posicion = 0;
     }
- private char CaracterActual => _posicion < _texto.Length ? _texto.Length ? _texto[_posicion] : '\0' ;
+ private char CaracterActual => _posicion < _texto.Length ? _texto[_posicion] : '\0';
 
  private void Avanzar() => _posicion++;
  private void SaltarEspacios()
@@ -74,4 +74,43 @@ private Nodo ParsearExpresion()
         return nodo;
     }
 
+    private Nodo ParsearFactor()
+    {
+        SaltarEspacios();
+
+        if (CaracterActual == "+")
+        {
+            Avanzar();
+            return new PositivoNodo(ParsearFactor());
+        }
+         if (CaracterActual == "-")
+        {
+            Avanzar();
+            return new NegativoNodo(ParsearFactor());
+        }
+         if (CaracterActual == "(")
+        {
+            Avanzar();
+            var nodo = ParsearExpresion();
+            SaltarEspacios();
+            if(CaracterActual != ")") throw new Exception("Error: Parentesis sin cerrar.");
+            Avanzar();
+            return nodo;
+        }
+        if (CaracterActual == 'x' || CaracterActual == 'X')
+        {
+            Avanzar();
+            return new varNodo();
+
+        }
+        if (char.IsDigit(CaracterActual))
+        {
+            int inicio = _posicion;
+            while (char.IsDigit(CaracterActual)) Avanzar();
+            string numeroStr = _texto.Substring(inicio, _posicion - inicio);
+            return new NumNodo(int.Parse(numeroStr));
+        }
+        throw new Exception($"Error : token inesperado '{CaracterActual}' en la posicion {_posicion}");
+
+    }
 }
