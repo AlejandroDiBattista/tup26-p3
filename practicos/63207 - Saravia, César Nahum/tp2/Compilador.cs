b87f316c19a8198a59dfa1b18587a9d883b41f1c
Nahum-Saravia <nahum-s@hotmail.com>
2026-04-22 15:37:12 -0300

    Implementar el parser para convertir expresiones en un AST con soporte para suma, resta, multiplicación y división

--- practicos/63207 - Saravia, César Nahum/tp2/Compilador.cs
+++ practicos/63207 - Saravia, César Nahum/tp2/Compilador.cs
@@ -1,5 +1,47 @@
+using System;
 class Compilador {
+    private string input = "";
+    private int pos = 0;
     public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+        if (string.IsNullOrWhiteSpace(expresion))
+            throw new FormatException("Token inesperado");
+        var comp = new Compilador();
+        comp.input = expresion;
+        comp.pos = 0;
+        var nodo = comp.ParseExpresion();
+        return nodo;
     }
-}
+    private Nodo ParseExpresion() {
+        Nodo nodo = ParseTermino();
+        while (true) {
+            SkipEspacios();
+            if (Match('+')) {
+                nodo = new SumaNodo(nodo, ParseTermino());
+            }
+            else if (Match('-')) {
+                nodo = new RestaNodo(nodo, ParseTermino());
+            }
+            else {
+                break;
+            }
+        }
+        return nodo;
+    }
+    private Nodo ParseTermino() {
+        Nodo nodo = ParseFactor();
+        while (true) {
+            SkipEspacios();
+            if (Match('*')) {
+                nodo = new MultiplicacionNodo(nodo, ParseFactor());
+            }
+            else if (Match('/')) {
+                nodo = new DivisionNodo(nodo, ParseFactor());
+            }
+            else {
+                break;
+            }
+        }
+
+        return nodo;
+    }
+    }
