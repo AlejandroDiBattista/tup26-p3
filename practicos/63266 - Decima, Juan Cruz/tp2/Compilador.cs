2763713db918f03592a9fff8476cf9e2f34ae50e
Juan Cruz Decima <devjuancruz003@gmail.com>
2026-04-22 17:12:37 -0300

    Nodo parse implementado

--- practicos/63266 - Decima, Juan Cruz/tp2/Compilador.cs
+++ practicos/63266 - Decima, Juan Cruz/tp2/Compilador.cs
@@ -1,5 +1,22 @@
-class Compilador {
-    public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+class Compilador
+{
+
+    private readonly string _expresion;
+    private int _pos;
+
+
+    public static Nodo Parse(string expresion)
+    {
+        var compilador = new Compilador(expresion);
+        var nodo = compilador.ParseExpresion();
+
+        compilador.SaltarEspacios();
+        if (compilador._pos < compilador._expresion.Length)
+        {
+            throw new FormatException(
+                $"Token inesperado: '{compilador._expresion[compilador._pos]}'");
+        }
+
+        return nodo;
     }
 }
