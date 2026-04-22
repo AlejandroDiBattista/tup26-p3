af8c676a136f8f724195f0e829dca5fc5b882a8d
bnjavila07 <bnjavilapuntano@gmail.com>
2026-04-21 21:50:56 -0300

    Actualizacion de sintaxis y agregado de comentarios

--- practicos/63313 - Ávila Puntano, Benjamín/tp2/Nodos.cs
+++ practicos/63313 - Ávila Puntano, Benjamín/tp2/Nodos.cs
@@ -1,51 +1,66 @@
 using System.Security.Cryptography.X509Certificates;
-
+// se usa abstract ya que hay varias formas de calcular un nudo : suma,resta, etc.
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
 
+// creamos una clase en base a nodo y se re define el comportamiento de evaluar
+// usamos el override para que se use la version de evaluar de esta clase y no de nodo
 class numero (int valor) : Nodo{
-    public override int evaluar ( int x = 0) => valor; 
+    public override int Evaluar ( int x = 0) => valor; 
 }
 
+//similar a la anterior clase, se devuelve directamente el valor que toma X
 class variable : Nodo
 {
     public override int Evaluar(int x = 0) => x;
 }
 
-abstract class binario(Nodo izquierdo, Nodo derecha) : Nodo
+//usamos el readonly para que sea inmutable estos dos nodos, binario es abstracto por que solo define la estructura y no las operaciones.
+abstract class Binario(Nodo izquierdo, Nodo derecho) : Nodo
 {
     protected readonly Nodo izquierdo = izquierdo; 
-    protected readonly Nodo derecha = derecha;
+    protected readonly Nodo derecho = derecho;
 }
 
-class Suma(Nodo izquierdo, Nodo derecho) : binario(izquierdo,derecha)
+// suma es un binario donde se usa los nodos izq,der para hacer una suma entre las dos
+class Suma(Nodo izquierdo, Nodo derecho) : binario(izquierdo,derecho)
 {
-    public override int evaluar(int x=0) => izquierdo.Evaluar(x) + derecha.Evaluar(x);
+    public override int Evaluar (int x=0) => izquierdo.Evaluar(x) + derecho.Evaluar(x);
 }
 
-class resta(Nodo izquierdo, Nodo derecho) : binario(izquierdo, derecha)
+//resta es un bionario que al igual que suma usa los nodos en una resta
+class Resta(Nodo izquierdo, Nodo derecho) : binario(izquierdo, derecho)
 {
-    public override int evaluar (int x=0) => izquierdo.Evaluar(x) - derecha.Evaluar(x);
+    public override int Evaluar (int x=0) => izquierdo.Evaluar(x) - derecho.Evaluar(x);
 
 }
 
-class multiplicacionproduc(Nodo izquierdo, Nodo derecho) : binario(izquierdo, derecha)
+// el producto es un binario que multiplica los dos nodos entre si
+class Multiplicacionprod(Nodo izquierdo, Nodo derecho) : binario(izquierdo, derecho)
 {
-    public override int Evaluar (int x=0) => izquierdo.Evaluar(x) * derecha.Evaluar(x);
+    public override int Evaluar (int x=0) => izquierdo.Evaluar(x) * derecho.Evaluar(x);
 
 }
 
-class division(Nodo izquierdo, Nodo derecho) : binario(izquierdo, derecha)
+// divison es un binario que divide los dos  resultados de los nodos, si el divisor(nodo derecho) es 0 genera un error y si no retorna la division de los dos nodos 
+class Division(Nodo izquierdo, Nodo derecho) : binario(izquierdo, derecho)
 {
     public override int Evaluar(int x = 0)
     {
-            variable divisor = derecha.Evaluar(x = 0);
+            int divisor = derecho.Evaluar(x);
 
-            if (divisor => 0) 
-            throw new DivideByZeroException ("es division por cero");
-            return izquierda.evaluar(x) / divisor;
+            if (divisor == 0) 
+            throw new DivideByZeroException ("es Division por cero");
+            return izquierdo.Evaluar(x) / divisor;
     }
 
     
     }
+    //
+    
+ class N_negativos(Nodo operando) : Nodo
+{
+    public override int Evaluar(int x = 0) => -operando.Evaluar(x);
+  
+}
