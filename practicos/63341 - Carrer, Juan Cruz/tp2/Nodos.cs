c92b972f655eaa315d66d6b93e3af67335f07bba
juancarrer11 <juancarrer11@gmail.com>
2026-04-22 15:11:55 -0300

    Definir estructura base de nodos

--- practicos/63341 - Carrer, Juan Cruz/tp2/Nodos.cs
+++ practicos/63341 - Carrer, Juan Cruz/tp2/Nodos.cs
@@ -1,3 +1,15 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
+
+class Numero : Nodo
+{
+    private int valor;
+    public Numero(int valor) => this.valor = valor;
+    public override int Evaluar(int x = 0) => valor;
+}
+
+class Variable : Nodo
+{
+    public override int Evaluar(int x = 0) => x;
+}
