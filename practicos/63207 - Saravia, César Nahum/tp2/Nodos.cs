18273a199cec86319d642b462bb81ac90dab9f59
Nahum-Saravia <nahum-s@hotmail.com>
2026-04-21 21:08:03 -0300

    Agregar clases MultiplicacionNodo y DivisionNodo para operaciones aritméticas en la estructura de nodos

--- practicos/63207 - Saravia, César Nahum/tp2/Nodos.cs
+++ practicos/63207 - Saravia, César Nahum/tp2/Nodos.cs
@@ -49,3 +49,21 @@ class RestaNodo : NodoBinario {
         return izquierdo.Evaluar(x) - derecho.Evaluar(x);
     }
 }
+
+class MultiplicacionNodo : NodoBinario {
+    public MultiplicacionNodo(Nodo izq, Nodo der) : base(izq, der) {}
+
+    public override int Evaluar(int x = 0) {
+        return izquierdo.Evaluar(x) * derecho.Evaluar(x);
+    }
+}
+class DivisionNodo : NodoBinario {
+    public DivisionNodo(Nodo izq, Nodo der) : base(izq, der) {}
+    public override int Evaluar(int x = 0) {
+        int divisor = derecho.Evaluar(x);
+        if (divisor == 0)
+            throw new DivideByZeroException();
+            
+        return izquierdo.Evaluar(x) / divisor;
+    }
+}
