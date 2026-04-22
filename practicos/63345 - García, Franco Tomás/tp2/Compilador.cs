9707d883a5dd4c3af57ca4575d21e5da563aa18d
Franco Tomas Garcia <143847992+FrancoTms@users.noreply.github.com>
2026-04-22 15:36:40 -0300

    Agrego estructura base del compilador

--- practicos/63345 - García, Franco Tomás/tp2/Compilador.cs
+++ practicos/63345 - García, Franco Tomás/tp2/Compilador.cs
@@ -1,5 +1,21 @@
 class Compilador {
+    private string input = "";
+    private int pos = 0;
+
     public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+        if (string.IsNullOrWhiteSpace(expresion)) throw new FormatException ("Token inesperado");
+
+        var p = new Compilador
+        {
+            input = expresion,
+            pos = 0
+        };
+
+        var nodo = p.ParseExpresion();
+
+        p.SaltarEspacios();
+        if (p.pos < p.input.Length) throw new FormatException("Token inesperado");
+
+        return nodo;
     }
 }
