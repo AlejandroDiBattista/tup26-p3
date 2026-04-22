0c1352b6979a3ebff1b1e01397f9de5ce13e0da4
Juli Galvan <jrgalvan1996@gmail.com>
2026-04-22 19:26:08 -0300

    se agrega la clase VariableNodo para el manejo de la variable X

--- practicos/63737 - Galván, Rocío Julieta/tp2/Compilador.cs
+++ practicos/63737 - Galván, Rocío Julieta/tp2/Compilador.cs
@@ -3,3 +3,6 @@ class Compilador {
         throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
     }
 }
+class VariableNodo : Nodo {
+    public override int Evaluar(int x) => x;
+}
