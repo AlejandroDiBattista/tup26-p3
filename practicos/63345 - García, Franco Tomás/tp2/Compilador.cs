fe37e7fdbb5d86bdfc92ec112dee9a45ef85ee1d
Franco Tomas Garcia <143847992+FrancoTms@users.noreply.github.com>
2026-04-22 15:58:50 -0300

    Implemento parsing de términos (multiplicación y división)

--- practicos/63345 - García, Franco Tomás/tp2/Compilador.cs
+++ practicos/63345 - García, Franco Tomás/tp2/Compilador.cs
@@ -43,4 +43,25 @@ class Compilador {
         }
         return nodo;
     }
+    private Nodo ParseTermino()
+    {
+        var nodo = ParseFactor();
+        while (true)
+        {
+            SaltarEspacios();
+            if (Match('*'))
+            {
+                nodo = new Multiplicacion(nodo, ParseFactor());
+            }
+            else if (Match('/'))
+            {
+                nodo = new Division(nodo, ParseFactor());
+            }
+            else
+            {
+                break;
+            }
+        }
+        return nodo;
+    }
 }
