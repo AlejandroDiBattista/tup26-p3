662447aab5c18638422df1deb078e47aa1263022
fabriciolazarte27 <fabriciolazarte.lf7@gmail.com>
2026-04-22 19:16:53 -0300

    Estructura base

--- practicos/63219 - Lazarte, Sergio Fabricio/tp2/Nodos.cs
+++ practicos/63219 - Lazarte, Sergio Fabricio/tp2/Nodos.cs
@@ -1,3 +1,33 @@
-abstract class Nodo {
+abstract class Nodo
+{
     public abstract int Evaluar(int x = 0);
+}
+
+class NumeroNodo : Nodo
+{
+    private int valor;
+
+    public NumeroNodo(int valor)
+    {
+        this.valor = valor;
+    }
+
+    public override int Evaluar(int x = 0) => valor;
+}
+
+class VariableNodo : Nodo
+{
+    public override int Evaluar(int x = 0) => x;
+}
+
+abstract class NodoBinario : Nodo
+{
+    protected Nodo izquierdo;
+    protected Nodo derecho;
+
+    public NodoBinario(Nodo izq, Nodo der)
+    {
+        izquierdo = izq;
+        derecho = der;
+    }
 }
