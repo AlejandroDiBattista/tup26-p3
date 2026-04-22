417fdf7367f71c1226e07611e93abce1a9f064cc
lumedina23 <lourdesnatalimedina348@gmail.com>
2026-04-22 15:12:27 -0300

    Agrega las clases NumeroNodo y VariableNodo con implementación del método Evaluar

--- practicos/63494 - Medina, Lourdes Natalia/tp2/Nodos.cs
+++ practicos/63494 - Medina, Lourdes Natalia/tp2/Nodos.cs
@@ -4,3 +4,16 @@ using System.ComponentModel;
 abstract class Nodo {
     public abstract int Evaluar(int x);
 }
+class NumeroNodo : Nodo {
+    private int valor;
+
+    public NumeroNodo(int valor) {
+        this.valor = valor;
+    }
+
+    public override int Evaluar(int x) => valor;
+}
+
+class VariableNodo : Nodo {
+    public override int Evaluar(int x) => x;
+}
