aa396ade309898549bc176686c2829e041862db2
hernancarpalvarez28 <hernancarpalvarez@gmail.com>
2026-04-22 16:08:39 -0300

    Creo clase abstracta nodo y los nodos hijos para evaluar los numeros.

--- practicos/63300 - Álvarez, Hernán Fabricio/tp2/Nodos.cs
+++ practicos/63300 - Álvarez, Hernán Fabricio/tp2/Nodos.cs
@@ -1,3 +1,30 @@
+using System;
+
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
+}
+
+abstract class NumNodo : Nodo
+{
+    public int Valor {get;}
+    public NumNodo(int valor) => Valor = valor;
+
+    public override int Evaluar(int x = 0) => Valor;
+}
+abstract class varNodo : Nodo
+{
+    public override int Evaluar(int x = 0) => x;
+}
+abstract class PositivoNodo : Nodo
+{
+    public Nodo hijo {get;}
+    public PositivoNodo(Nodo hijo) => hijo = hijo;
+    public override int Evaluar(int x = 0) => Hijo.Evaluar(x);
+}
+abstract class NegativoNodo : Nodo
+{
+    public Nodo Hijo {get;}
+    public NegativoNodo(Nodo hijo) => Hijo = hijo;
+
+    public override int Evaluar (int x = 0) => -Hijo.Evaluar(x);
 }
