af747968d6e080ff13a0af2cf4a2cb9adbbcdd9b
Facundo Morán <facumamoran@gmail.com>
2026-04-21 22:48:40 -0300

    Agrego nodos binarios (suma, resta, multiplicación y división)

--- practicos/63412 - Morán, Facundo Martín/tp2/Nodos.cs
+++ practicos/63412 - Morán, Facundo Martín/tp2/Nodos.cs
@@ -14,3 +14,21 @@ abstract class NodoBinario(Nodo izquierda, Nodo derecha) : Nodo {
     protected readonly Nodo Izquierda = izquierda;
     protected readonly Nodo Derecha = derecha;
 }
+class SumaNodo(Nodo izquierda, Nodo derecha) : NodoBinario(izquierda, derecha) {
+    public override int Evaluar(int x = 0) => Izquierda.Evaluar(x) + Derecha.Evaluar(x);
+}
+class RestaNodo(Nodo izquierda, Nodo derecha) : NodoBinario(izquierda, derecha) {
+    public override int Evaluar(int x = 0) => Izquierda.Evaluar(x) - Derecha.Evaluar(x);
+}
+class MultiplicacionNodo(Nodo izquierda, Nodo derecha) : NodoBinario(izquierda, derecha) {
+    public override int Evaluar(int x = 0) => Izquierda.Evaluar(x) * Derecha.Evaluar(x);
+}
+class DivisionNodo(Nodo izquierda, Nodo derecha) : NodoBinario(izquierda, derecha) {
+    public override int Evaluar(int x = 0)
+    {
+        var divisor = Derecha.Evaluar(x);
+        if (divisor == 0)
+            throw new DivideByZeroException("División por cero no permitida.");
+        return Izquierda.Evaluar(x) / divisor;
+    }
+}
