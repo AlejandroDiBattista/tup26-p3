1cf2a0c487e16dd4b828a75fe695eb315185c2b5
suarezsofia541-alt <suarezsofia541@gmail.com>
2026-04-22 14:22:55 -0300

    VariableNodo y NegativoNodo
    variablenodo es el lugar donde se guarda la variable x y negativonodo es donde se guardan los numeros negativos, nagativonodo le pregunta a evaluar que valor tiene y a ese valor le pone un signo negativo

--- practicos/63447 - Suárez, Sofía Celeste/tp2/Nodos.cs
+++ practicos/63447 - Suárez, Sofía Celeste/tp2/Nodos.cs
@@ -9,3 +9,13 @@ class NumeroNodo : Nodo {
     public NumeroNodo(int valor) => _valor = valor;
     public override int Evaluar(int x = 0) => _valor;
 }
+
+class VariableNodo : Nodo {
+    public override int Evaluar(int x = 0) => x;
+}
+
+class NegativoNodo : Nodo {
+    private readonly Nodo _hijo;
+    public NegativoNodo(Nodo hijo) => _hijo = hijo;
+    public override int Evaluar(int x = 0) => -_hijo.Evaluar(x);
+}
