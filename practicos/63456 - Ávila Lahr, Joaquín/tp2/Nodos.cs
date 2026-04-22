f4ecaeda8d91aab6b068b54e467b20b2f477a602
Joacoavila9 <joaquinavilalahr330@gmail.com>
2026-04-22 20:24:26 -0300

    aplicamos en nodos.cs la estructura recomendad y empezamos con la definicion de las clases y los metodos dentro de cada uno

--- practicos/63456 - Ávila Lahr, Joaquín/tp2/Nodos.cs
+++ practicos/63456 - Ávila Lahr, Joaquín/tp2/Nodos.cs
@@ -1,3 +1,42 @@
-abstract class Nodo {
-    public abstract int Evaluar(int x = 0);
+abstract class Nodo
+{
+    public abstract int Evaluar(int x);
 }
+class NumeroNodo : Nodo
+{
+    public int Valor;
+    public NumeroNodo(int valor) => Valor = valor;
+    public override int Evaluar(int x) => Valor;
+}
+class VariableNodo : Nodo
+{
+    public override int Evaluar(int x) => x;
+}
+class NegativoNodo : Nodo
+{
+
+}
+abstract class NodoBinario : Nodo
+{
+
+}
+class SumaNodo : NodoBinario
+{
+ 
+}
+class RestaNodo : NodoBinario
+{
+
+}
+class MultiplicacionNodo : NodoBinario
+{
+    
+}
+class DivisionNodo : NodoBinario
+{
+
+}
+
+
+
+
