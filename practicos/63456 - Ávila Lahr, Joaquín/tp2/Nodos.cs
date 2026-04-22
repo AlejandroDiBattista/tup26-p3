608b6cd0360bd807d0be7028b10c6e9cd512dba2
Joacoavila9 <joaquinavilalahr330@gmail.com>
2026-04-22 20:46:33 -0300

    termino el nodos.cs

--- practicos/63456 - Ávila Lahr, Joaquín/tp2/Nodos.cs
+++ practicos/63456 - Ávila Lahr, Joaquín/tp2/Nodos.cs
@@ -14,29 +14,46 @@ class VariableNodo : Nodo
 }
 class NegativoNodo : Nodo
 {
-
+    public Nodo Expr;
+    public NegativoNodo(Nodo expr) => Expr = expr;
+    public override int Evaluar(int x) => -Expr.Evaluar(x);
 }
 abstract class NodoBinario : Nodo
 {
+    public Nodo Izq;
+    public Nodo Der;
 
+    public NodoBinario(Nodo izq, Nodo der)
+    {
+        Izq = izq;
+        Der = der;
+    }
 }
 class SumaNodo : NodoBinario
 {
- 
+    public SumaNodo(Nodo izq, Nodo der) : base(izq, der) { }
+    public override int Evaluar(int x) => Izq.Evaluar(x) + Der.Evaluar(x);
 }
 class RestaNodo : NodoBinario
 {
-
+    public RestaNodo(Nodo izq, Nodo der) : base(izq, der) { }
+    public override int Evaluar(int x) => Izq.Evaluar(x) - Der.Evaluar(x);
 }
 class MultiplicacionNodo : NodoBinario
 {
-    
+    public MultiplicacionNodo(Nodo izq, Nodo der) : base(izq, der) { }
+    public override int Evaluar(int x) => Izq.Evaluar(x) * Der.Evaluar(x);
 }
 class DivisionNodo : NodoBinario
 {
+    public DivisionNodo(Nodo izq, Nodo der) : base(izq, der) { }
+    public override int Evaluar(int x)
+    {
+        int divisor = Der.Evaluar(x);
 
+        if (divisor == 0)
+            throw new DivideByZeroException("División por cero");
+
+        return Izq.Evaluar(x) / divisor;
+    }
 }
-
-
-
-
