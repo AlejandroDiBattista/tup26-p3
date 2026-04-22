8435d78926b894fbcdbf972e9b21fdac434bb5d0
LucianoLeonelGuzman <guzmanlucianoleonel@gmail.com>
2026-04-22 20:23:57 -0300

    Agregue la clase NumeroNodo para representar los literales enteros, la clase VariableNodo para la variable 'x' y la clase Nodounario que es la base para binarios y sus derivados NegativoNodo y PositivoNodo

--- practicos/62844 - Guzmán, Luciano Leonel/tp2/Nodos.cs
+++ practicos/62844 - Guzmán, Luciano Leonel/tp2/Nodos.cs
@@ -1,3 +1,32 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
+//--clase NumeroNodo--------------------------------
+class NumeroNodo : Nodo {
+    public int Valor { get; }
+
+    public NumeroNodo(int valor) {
+        Valor = valor;
+    }
+
+    public override int Evaluar(int x = 0) => Valor;
+}
+//-------clase VariableNodo--------------------------------
+class VariableNodo : Nodo {
+    public override int Evaluar(int x = 0) => x;
+}
+abstract class NodoUnario : Nodo {
+    protected Nodo Operando;
+
+    public NodoUnario(Nodo operando) {
+        Operando = operando;
+    }
+}
+class NegativoNodo : NodoUnario {
+    public NegativoNodo(Nodo operando) : base(operando) { }
+    public override int Evaluar(int x = 0) => -Operando.Evaluar(x);
+}
+class PositivoNodo : NodoUnario {
+    public PositivoNodo(Nodo operando) : base(operando) { }
+    public override int Evaluar(int x = 0) => Operando.Evaluar(x);
+}
