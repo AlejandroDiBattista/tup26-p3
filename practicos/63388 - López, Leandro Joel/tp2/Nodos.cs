3380b5f24bae4329ca52e745f7935ece38380ceb
Leandro Lopez <djlean04052006@gmail.com>
2026-04-22 19:51:51 -0300

    La estructura base del AST

--- practicos/63388 - López, Leandro Joel/tp2/Nodos.cs
+++ practicos/63388 - López, Leandro Joel/tp2/Nodos.cs
@@ -1,3 +1,18 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
+
+class NumeroNodo : Nodo {
+    private readonly int valor;
+
+    public NumeroNodo(int valor) {
+        this.valor = valor;
+    }
+
+    public override int Evaluar(int x = 0) => valor;
+    
+}
+
+class VariableNodo : Nodo {
+    public override int Evaluar(int x = 0) => x;
+}
