b22aec1e0fcbc4e0dad95c466d019f8373ea470f
sofisanchez126 <sofisanchez3333@gmail.com>
2026-04-22 12:36:30 -0300

    Completa parser con manejo de suma, resta y validaciones de parsing

--- practicos/63216 - Sánchez, Sofía Micaela/tp2/Compilador.cs
+++ practicos/63216 - Sánchez, Sofía Micaela/tp2/Compilador.cs
@@ -1,18 +1,33 @@
 class Compilador
-{ 
-     private string texto;
-     private int pos;
+{
+    private string texto;
+    private int pos;
 
     private Compilador(string texto)
     {
         this.texto = texto.Replace(" ", "");
         pos = 0;
     }
+
     public static Nodo Parse(string expresion)
     {
+        if (string.IsNullOrWhiteSpace(expresion))
+            throw new FormatException("Token inesperado");
+
         var comp = new Compilador(expresion);
-        return comp.ParseTermino();
+        Nodo nodo = comp.ParseExpresion();
+
+        if (!comp.Fin())
+            throw new FormatException("Token inesperado");
+
+        return nodo;
     }
+
+    private bool Fin()
+    {
+        return pos >= texto.Length;
+    }
+
     private char Actual()
     {
         return pos < texto.Length ? texto[pos] : '\0';
@@ -21,6 +36,26 @@ class Compilador
     private void Avanzar()
     {
         pos++;
+    }
+
+    private Nodo ParseExpresion()
+    {
+        Nodo nodo = ParseTermino();
+
+        while (Actual() == '+' || Actual() == '-')
+        {
+            char op = Actual();
+            Avanzar();
+
+            Nodo derecho = ParseTermino();
+
+            if (op == '+')
+                nodo = new SumaNodo(nodo, derecho);
+            else
+                nodo = new RestaNodo(nodo, derecho);
+        }
+
+        return nodo;
     }
 
     private Nodo ParseTermino()
@@ -43,7 +78,6 @@ class Compilador
         return nodo;
     }
 
-
     private Nodo ParseFactor()
     {
         char c = Actual();
@@ -53,18 +87,25 @@ class Compilador
             Avanzar();
             return ParseFactor();
         }
+
         if (c == '-')
         {
             Avanzar();
             return new NegativoNodo(ParseFactor());
         }
+
         if (c == '(')
         {
             Avanzar();
-            var nodo = ParseTermino();
-            Avanzar(); 
+            Nodo nodo = ParseExpresion();
+
+            if (Actual() != ')')
+                throw new FormatException("Se esperaba ')'");
+
+            Avanzar();
             return nodo;
         }
+
         if (char.IsDigit(c))
         {
             int inicio = pos;
@@ -75,11 +116,13 @@ class Compilador
             int valor = int.Parse(texto.Substring(inicio, pos - inicio));
             return new NumeroNodo(valor);
         }
+
         if (c == 'x' || c == 'X')
         {
             Avanzar();
             return new VariableNodo();
         }
+
         throw new FormatException("Token inesperado");
     }
 }
