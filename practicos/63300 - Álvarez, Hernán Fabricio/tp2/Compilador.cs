18c5c2092cc21f2dd170ff367634b4b9d9861873
hernancarpalvarez28 <hernancarpalvarez@gmail.com>
2026-04-22 17:33:38 -0300

    En Compilador agregue metodos avanzar caracteractual y saltarespacios para leer letra por letra y saltear espacios en blanco

--- practicos/63300 - Álvarez, Hernán Fabricio/tp2/Compilador.cs
+++ practicos/63300 - Álvarez, Hernán Fabricio/tp2/Compilador.cs
@@ -1,5 +1,39 @@
-class Compilador {
-    public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+using System;
+using System.Runtime.CompilerServices;
+
+
+public class Compilador {
+    
+    private readonly string _texto;
+    private int _posicion;
+    public Compilador(string texto)
+    {
+        if(string.IsNullOrWhiteSpace(texto))
+         throw new Exception("Error: dato de entrada vacio.");
+         _texto = texto;
+         _posicion = 0;
     }
+ private char CaracterActual => _posicion < _texto.Length ? _texto.Length ? _texto[_posicion] : '\0' ;
+
+ private void Avanzar() => _posicion++;
+ private void SaltarEspacios()
+    {
+        while (char.IsWhiteSpace(CaracterActual)) Avanzar();
+    }   
+
+public Nodo Parsear()
+    {
+        SaltarEspacios();
+        if (CaracterActual == '\0')
+           throw new Exception("Error: Entrada vacia.");
+        
+        var ast = ParsearExpresion();
+
+        SaltarEspacios();
+        if(CaracterActual != '\0')
+            throw new Exception($"Error token Inesperado '{CaracterActual}' en la posicion {_posicion}.");
+
+        return ast;
+    }
+    
 }
