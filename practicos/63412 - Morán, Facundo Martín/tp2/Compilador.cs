bbaf38fa36864ed85b1a5b0413b3397255bf7d9d
Facundo Morán <facumamoran@gmail.com>
2026-04-22 00:03:24 -0300

    Agrego la estructura base del compilador

--- practicos/63412 - Morán, Facundo Martín/tp2/Compilador.cs
+++ practicos/63412 - Morán, Facundo Martín/tp2/Compilador.cs
@@ -1,4 +1,15 @@
 class Compilador {
+    private readonly string _input;
+    private int _cursor;
+    private Compilador(string input) {
+        _input = input;
+        _cursor = 0;
+    }
+    private void IgnorarEspacios() {
+        while (_cursor < _input.Length && char.IsWhiteSpace(_input[_cursor])) {
+            _cursor++;
+        }
+    }
     public static Nodo Parse(string expresion) {
         throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
     }
