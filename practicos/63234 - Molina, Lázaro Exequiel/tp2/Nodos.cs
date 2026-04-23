8813fee4bbbbfc15599290cee35fae75790c08c3
Lazaro2005-Ml <molinalazarou@gmail.com>
2026-04-22 22:53:21 -0300

    Cree la variable Nodo,Nodo negativo y el nodo binario

--- practicos/63234 - Molina, Lázaro Exequiel/tp2/Nodos.cs
+++ practicos/63234 - Molina, Lázaro Exequiel/tp2/Nodos.cs
@@ -17,3 +17,38 @@ class NoNumero : Nodo
         return valor;
     }
 }
+
+class NoVariable : Nodo
+{
+    public override int Evaluar(int x = 0)
+    {
+        return x;
+    }
+}
+
+class NoNegativo : Nodo
+{
+    private Nodo nodo;
+
+    public NoNegativo(Nodo nodo)
+    {
+        this.nodo = nodo;
+    }
+
+    public override int Evaluar(int x = 0)
+    {
+        return -nodo.Evaluar(x);
+    }
+}
+
+abstract class NoBinario : Nodo
+{
+    protected Nodo izquierda;
+    protected Nodo derecha;
+
+    public NoBinario(Nodo izq, Nodo der)
+    {
+        izquierda = izq;
+        derecha = der;
+    }
+}
