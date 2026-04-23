ebe7133272774827eeb0149c3c7f8c9d66aec7b0
JoseDiaz <josesimondiaz13@gmail.com>
2026-04-22 23:31:57 -0300

    feat: agregar NumeroNodo y VariableNodo como nodos hoja del AST

--- practicos/63137 - Díaz, José Simón/tp2/Nodos.cs
+++ practicos/63137 - Díaz, José Simón/tp2/Nodos.cs
@@ -1,3 +1,11 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
+
+class NumeroNodo(int valor) : Nodo {
+    public override int Evaluar(int x = 0) => valor;
+}
+
+class VariableNodo : Nodo {
+    public override int Evaluar(int x = 0) => x;
+}
