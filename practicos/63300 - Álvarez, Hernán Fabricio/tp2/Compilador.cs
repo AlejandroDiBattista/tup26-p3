fa5f9182b9af46be8fa1ba03bb7f930ffca66b12
hernancarpalvarez28 <hernancarpalvarez@gmail.com>
2026-04-22 21:28:13 -0300

    Tuve que pedirle a la ia que me arreglara el codigo por errores del terminal

--- practicos/63300 - Álvarez, Hernán Fabricio/tp2/Compilador.cs
+++ practicos/63300 - Álvarez, Hernán Fabricio/tp2/Compilador.cs
@@ -5,68 +5,62 @@ using System.Xml;
 
 
 public class Compilador {
-    
     private readonly string _texto;
     private int _posicion;
-    public static Nodo Parse (string expresion)
-    {
+
+    public static Nodo Parse(string expresion) {
         return new Compilador(expresion).Parsear();
     }
-    public Compilador(string texto)
-    {
-        if(string.IsNullOrWhiteSpace(texto))
-         throw new Exception("Error: dato de entrada vacio.");
-         _texto = texto ?? "";
-         _posicion = 0;
+
+    private Compilador(string texto) {
+        _texto = texto ?? "";
+        _posicion = 0;
     }
- private char CaracterActual => _posicion < _texto.Length ? _texto[_posicion] : '\0';
 
- private void Avanzar() => _posicion++;
- private void SaltarEspacios()
-    {
+    private char CaracterActual => _posicion < _texto.Length ? _texto[_posicion] : '\0';
+
+    private void Avanzar() => _posicion++;
+
+    private void SaltarEspacios() {
         while (char.IsWhiteSpace(CaracterActual)) Avanzar();
-    }   
+    }
 
-public Nodo Parsear()
-    {
+    private Nodo Parsear() {
         SaltarEspacios();
-        if (CaracterActual == '\0')
-           throw new FormatException("Token inesperado :  Entrada vacia.");
-        
+        if (CaracterActual == '\0') 
+            throw new FormatException("Token inesperado: entrada vacía");
+
         var ast = ParsearExpresion();
-
         SaltarEspacios();
-        if(CaracterActual != '\0')
-            throw new FormatException($"Error token Inesperado '{CaracterActual}' en la posicion {_posicion}.");
+        
+        if (CaracterActual != '\0')
+            throw new FormatException($"Token inesperado: '{CaracterActual}'");
 
         return ast;
     }
 
-private Nodo ParsearExpresion()
-    {
+    private Nodo ParsearExpresion() {
         var nodo = ParsearTermino();
         SaltarEspacios();
 
-        while (CaracterActual == '+' || CaracterActual == '-')
-        {
-            Char operador = CaracterActual;
+        while (CaracterActual == '+' || CaracterActual == '-') {
+            char operador = CaracterActual;
             Avanzar();
             var derecho = ParsearTermino();
 
-            if(operador == '+') nodo = new SumaNodo(nodo , derecho);
+            if (operador == '+') nodo = new SumaNodo(nodo, derecho);
             else nodo = new RestaNodo(nodo, derecho);
 
             SaltarEspacios();
         }
         return nodo;
     }
-    
-    private Nodo ParsearTermino()
-    {
+
+    private Nodo ParsearTermino() {
         var nodo = ParsearFactor();
         SaltarEspacios();
-        while (CaracterActual == '*' || CaracterActual == '/')
-        {
+
+        while (CaracterActual == '*' || CaracterActual == '/') {
             char operador = CaracterActual;
             Avanzar();
             var derecho = ParsearFactor();
@@ -79,43 +73,37 @@ private Nodo ParsearExpresion()
         return nodo;
     }
 
-    private Nodo ParsearFactor()
-    {
+    private Nodo ParsearFactor() {
         SaltarEspacios();
+        if (CaracterActual == '\0') throw new FormatException("Token inesperado");
 
-        if (CaracterActual == '+')
-        {
+        if (CaracterActual == '+') {
             Avanzar();
             return new PositivoNodo(ParsearFactor());
         }
-         if (CaracterActual == '-')
-        {
+        if (CaracterActual == '-') {
             Avanzar();
             return new NegativoNodo(ParsearFactor());
         }
-         if (CaracterActual == '(')
-        {
+        if (CaracterActual == '(') {
             Avanzar();
             var nodo = ParsearExpresion();
             SaltarEspacios();
-            if(CaracterActual !=  ')' ) throw new FormatException("Error: Parentesis sin cerrar.");
-            Avanzar();
+            if (CaracterActual != ')') throw new FormatException("Se esperaba ')'");
+            Avanzar(); 
             return nodo;
         }
-        if (CaracterActual == 'x' || CaracterActual == 'X')
-        {
+        if (CaracterActual == 'x' || CaracterActual == 'X') {
             Avanzar();
-            return new varNodo();
-
+            return new VariableNodo();
         }
-        if (char.IsDigit(CaracterActual))
-        {
+        if (char.IsDigit(CaracterActual)) {
             int inicio = _posicion;
             while (char.IsDigit(CaracterActual)) Avanzar();
             string numeroStr = _texto.Substring(inicio, _posicion - inicio);
-            return new NumNodo(int.Parse(numeroStr));
+            return new NumeroNodo(int.Parse(numeroStr));
         }
-        throw new Exception($"Error : token inesperado '{CaracterActual}' en la posicion {_posicion}");
 
+        throw new FormatException($"Token inesperado: '{CaracterActual}'");
     }
 }
