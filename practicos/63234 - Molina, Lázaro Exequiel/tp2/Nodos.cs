092d271a8ef15b9cb49bdec922da969ab8da241e
Lazaro2005-Ml <molinalazarou@gmail.com>
2026-04-22 22:52:30 -0300

    Cree la clase base de Nodo

--- practicos/63234 - Molina, Lázaro Exequiel/tp2/Nodos.cs
+++ practicos/63234 - Molina, Lázaro Exequiel/tp2/Nodos.cs
@@ -1,3 +1,19 @@
-abstract class Nodo {
+abstract class Nodo
+{
     public abstract int Evaluar(int x = 0);
+}
+
+class NoNumero : Nodo
+{
+    private int valor;
+
+    public NoNumero(int valor)
+    {
+        this.valor = valor;
+    }
+
+    public override int Evaluar(int x = 0)
+    {
+        return valor;
+    }
 }
