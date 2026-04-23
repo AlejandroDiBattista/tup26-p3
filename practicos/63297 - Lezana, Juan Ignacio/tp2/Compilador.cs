4077a35ca1078b5d53b2190ce0081224f0cc039e
IgnacioLezana25 <nacholezana09@gmail.com>
2026-04-22 23:12:49 -0300

    cree estructura del compilador y método parse

--- practicos/63297 - Lezana, Juan Ignacio/tp2/Compilador.cs
+++ practicos/63297 - Lezana, Juan Ignacio/tp2/Compilador.cs
@@ -1,5 +1,22 @@
-class Compilador {
-    public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+class Compilador
+{
+    private string texto;
+    private int posicion;
+
+    private Compilador(string expresion)
+    {
+        texto = expresion.Replace(" ", "");
+        posicion = 0;
+    }
+    public static Nodo Parse(string expresion)
+    {
+        var parser = new Compilador(expresion);
+
+        Nodo resultado = parser.Expresion();
+
+        if (parser.posicion != parser.texto.Length)
+            throw new FormatException("Token inesperado");
+
+        return resultado;
     }
 }
