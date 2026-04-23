8fdba670913e5aeb4b1dfd752160c446d7ab73fc
JoseDiaz <josesimondiaz13@gmail.com>
2026-04-22 23:33:18 -0300

    feat: agregar nodos unarios y binarios al AST

--- practicos/63137 - Díaz, José Simón/tp2/Nodos.cs
+++ practicos/63137 - Díaz, José Simón/tp2/Nodos.cs
@@ -9,3 +9,37 @@ class NumeroNodo(int valor) : Nodo {
 class VariableNodo : Nodo {
     public override int Evaluar(int x = 0) => x;
 }
+
+class NegativoNodo(Nodo operando) : Nodo {
+    public override int Evaluar(int x = 0) => -operando.Evaluar(x);
+}
+
+class PositivoNodo(Nodo operando) : Nodo {
+    public override int Evaluar(int x = 0) => operando.Evaluar(x);
+}
+
+abstract class NodoBinario(Nodo izquierdo, Nodo derecho) : Nodo {
+    protected Nodo Izquierdo { get; } = izquierdo;
+    protected Nodo Derecho   { get; } = derecho;
+}
+
+class SumaNodo(Nodo izquierdo, Nodo derecho) : NodoBinario(izquierdo, derecho) {
+    public override int Evaluar(int x = 0) => Izquierdo.Evaluar(x) + Derecho.Evaluar(x);
+}
+
+class RestaNodo(Nodo izquierdo, Nodo derecho) : NodoBinario(izquierdo, derecho) {
+    public override int Evaluar(int x = 0) => Izquierdo.Evaluar(x) - Derecho.Evaluar(x);
+}
+
+class MultiplicacionNodo(Nodo izquierdo, Nodo derecho) : NodoBinario(izquierdo, derecho) {
+    public override int Evaluar(int x = 0) => Izquierdo.Evaluar(x) * Derecho.Evaluar(x);
+}
+
+class DivisionNodo(Nodo izquierdo, Nodo derecho) : NodoBinario(izquierdo, derecho) {
+    public override int Evaluar(int x = 0) {
+        int divisor = Derecho.Evaluar(x);
+        if (divisor == 0)
+            throw new DivideByZeroException("División por cero.");
+        return Izquierdo.Evaluar(x) / divisor;
+    }
+}
