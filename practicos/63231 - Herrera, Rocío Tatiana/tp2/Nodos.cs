7dae0e11839f252be729ce5e02c973b156741490
tatianaherrera-commits <aatianaherrera350@gmail.com>
2026-04-22 22:49:46 -0300

    agrege la constante y la variable

--- practicos/63231 - Herrera, Rocío Tatiana/tp2/Nodos.cs
+++ practicos/63231 - Herrera, Rocío Tatiana/tp2/Nodos.cs
@@ -6,3 +6,14 @@ public abstract class Expresion
 { public abstract int Calcular(int x=0);
 }
  
+public class constante : Expresion
+{
+    private readonly int valor;
+    public constante(int valor) => valor=valor;
+    public override int Calcular(int x=0) => valor;
+}
+
+public class variable : Expresion
+{
+    public override int Calcular (int x=0) => x;
+}
