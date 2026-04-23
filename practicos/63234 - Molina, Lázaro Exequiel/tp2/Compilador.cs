ad47cd230924a046e643f7bb50a0f752dc7ea2b0
Lazaro2005-Ml <molinalazarou@gmail.com>
2026-04-22 22:57:50 -0300

    cree la clase y el constructor del compilador

--- practicos/63234 - Molina, Lázaro Exequiel/tp2/Compilador.cs
+++ practicos/63234 - Molina, Lázaro Exequiel/tp2/Compilador.cs
@@ -1,5 +1,10 @@
-class Compilador {
-    public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+class Compilador
+{
+    private string texto;
+    private int posicion;
+
+    private Compilador(string expresion)
+    {
+        texto = expresion.Replace(" ", "");
+        posicion = 0;
     }
-}
