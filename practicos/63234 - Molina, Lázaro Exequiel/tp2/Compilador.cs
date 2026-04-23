b8c468aa70d8a8f802b64151047bb2a1deb247cb
Lazaro2005-Ml <molinalazarou@gmail.com>
2026-04-22 22:58:15 -0300

    cree el metodo estatico (Parse)

--- practicos/63234 - Molina, Lázaro Exequiel/tp2/Compilador.cs
+++ practicos/63234 - Molina, Lázaro Exequiel/tp2/Compilador.cs
@@ -8,3 +8,14 @@ class Compilador
         texto = expresion.Replace(" ", "");
         posicion = 0;
     }
+public static Nodo Parse(string expresion)
+{
+    var parser = new Compilador(expresion);
+
+    Nodo resultado = parser.Expresion();
+
+    if (parser.posicion != parser.texto.Length)
+        throw new FormatException("Token inesperado");
+
+    return resultado;
+}
