09bc09cec51634e21dd837f624fdae5aed737b69
LucianoLeonelGuzman <guzmanlucianoleonel@gmail.com>
2026-04-22 22:16:56 -0300

    Estructura base del compilador y motor de análisis de texto

--- practicos/62844 - Guzmán, Luciano Leonel/tp2/Compilador.cs
+++ practicos/62844 - Guzmán, Luciano Leonel/tp2/Compilador.cs
@@ -1,5 +1,42 @@
+using System;
+
 class Compilador {
     public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+        if (string.IsNullOrWhiteSpace(expresion)) {
+            throw new FormatException("Token inesperado");
+        }
+
+        // Usamos la clase interna ConstructorAST para procesar la cadena
+        var constructor = new ConstructorAST(expresion);
+        var arbolRaiz = constructor.ParseExpresion();
+        constructor.SaltarEspacios();
+
+        if (!constructor.FinDeTexto) {
+            throw new FormatException("Token inesperado");
+        }
+
+        return arbolRaiz;
+    }
+
+    private class ConstructorAST {
+        private readonly string texto;
+        private int indice;
+
+        public ConstructorAST(string contenido) {
+            texto = contenido;
+            indice = 0;
+        }
+
+        public bool FinDeTexto => indice >= texto.Length;
+
+        private char VerActual() => FinDeTexto ? '\0' : texto[indice];
+
+        private char Avanzar() => FinDeTexto ? '\0' : texto[indice++];
+
+        public void SaltarEspacios() {
+            while (!FinDeTexto && char.IsWhiteSpace(VerActual())) {
+                indice++;
+            }
+        }
     }
 }
