081352a4f9fc488ad22febe0c7f4c6e82d3081d0
Bruno Busnelli <bbusnelli1@gmail.com>
2026-05-10 10:44:00 -0300

    agrega nodos para operaciones

--- practicos/63493 - Busnelli, Bruno/tp2/Nodos.cs
+++ practicos/63493 - Busnelli, Bruno/tp2/Nodos.cs
@@ -19,3 +19,48 @@ class NodoVariable : Nodo {
         return x;
     }
 }
+
+class NodoBinario : Nodo {
+    private readonly Nodo izquierda;
+    private readonly Nodo derecha;
+    private readonly char operador;
+
+    public NodoBinario(Nodo izquierda, char operador, Nodo derecha) {
+        this.izquierda = izquierda;
+        this.operador = operador;
+        this.derecha = derecha;
+    }
+
+    public override int Evaluar(int x = 0) {
+        int a = izquierda.Evaluar(x);
+        int b = derecha.Evaluar(x);
+
+        return operador switch {
+            '+' => a + b,
+            '-' => a - b,
+            '*' => a * b,
+            '/' => a / b,
+            _ => throw new InvalidOperationException("Operador inválido")
+        };
+    }
+}
+
+class NodoUnario : Nodo {
+    private readonly char operador;
+    private readonly Nodo nodo;
+
+    public NodoUnario(char operador, Nodo nodo) {
+        this.operador = operador;
+        this.nodo = nodo;
+    }
+
+    public override int Evaluar(int x = 0) {
+        int valor = nodo.Evaluar(x);
+
+        return operador switch {
+            '+' => valor,
+            '-' => -valor,
+            _ => throw new InvalidOperationException("Operador inválido")
+        };
+    }
+}
