84bd36e7e4150c7241f513c28d628c4f26055aef
Bruno Busnelli <bbusnelli1@gmail.com>
2026-05-10 10:42:15 -0300

    agrega nodos basicos del ast

--- practicos/63493 - Busnelli, Bruno/tp2/Nodos.cs
+++ practicos/63493 - Busnelli, Bruno/tp2/Nodos.cs
@@ -1,3 +1,21 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
+
+class NodoNumero : Nodo {
+    private readonly int valor;
+
+    public NodoNumero(int valor) {
+        this.valor = valor;
+    }
+
+    public override int Evaluar(int x = 0) {
+        return valor;
+    }
+}
+
+class NodoVariable : Nodo {
+    public override int Evaluar(int x = 0) {
+        return x;
+    }
+}
