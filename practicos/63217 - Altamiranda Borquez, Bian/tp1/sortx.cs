f21fb622629019c90938459ffa0edc177c7372c5
sofiaaltamiranda22 <sofiaaltamiranda30@gmail.com>
2026-05-14 09:59:50 -0300

    agregar parseo de argumentos y configuración inicial

--- practicos/63217 - Altamiranda Borquez, Bian/tp1/sortx.cs
+++ practicos/63217 - Altamiranda Borquez, Bian/tp1/sortx.cs
@@ -1,7 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Globalization;
 
-// sortx [input [output]] [-b|--by campo[:tipo[:orden]]]...
-//       [-i|--input input] [-o|--output output]
-//       [-d|--delimiter delimitador]
-//       [-nh|--no-header] [-h|--help]
-
-Console.WriteLine($"sortx {string.Join(" ", args)}");
+try{
+  var config = ParseArgs(args);
+}
+catch (Exception ex){
+    Console.Error.WriteLine($"Error: {ex.Message}");
+    Environment.Exit(1);
+}
+AppConfig ParseArgs(string[] args) {
+    string? input = null;
+    string? output = null;
+    string delimiter = ",";
+    bool noHeader = false;
+    var sortFields = new List<SortField>();
+    for (int i = 0; i < args.Length; i++){
+        var arg = args[i];
+        if (arg == "-i" || arg == "--input")
+            input = args[++i];
+        else if (arg == "-o" || arg == "--output")
+            output = args[++i];
+        else if (arg == "-d" || arg == "--delimiter")
+        {
+            var val = args[++i];
+            delimiter = val == "\\t" ? "\t" : val;
+        }
+        else if (arg == "-nh" || arg == "--no-header")
+            noHeader = true;
+        else if (arg == "-b" || arg == "--by")
+            sortFields.Add(ParseSortField(args[++i]));
+        else if (arg == "-h" || arg == "--help"){
+            PrintHelp();
+            Environment.Exit(0);
+        }
+    }
+    return new AppConfig(input, output, delimiter, noHeader, sortFields);
+}
