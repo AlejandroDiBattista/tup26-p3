aa1ca2b08214e88a31d92d7c657a6616367be1d9
IgnacioLezana25 <nacholezana09@gmail.com>
2026-05-22 20:52:46 -0300

    agregar contactos y guardar en base de datos

--- practicos/63297 - Lezana, Juan Ignacio/tp3/agenda.cs
+++ practicos/63297 - Lezana, Juan Ignacio/tp3/agenda.cs
@@ -15,3 +15,46 @@ using Microsoft.Data.Sqlite;
 using Dapper;
 using System.Data.Common;
 using Dapper.Contrib.Extensions;
+
+[Table("Contactos")]
+public sealed class Ctc {
+    [Key] public int Id { get; set; }
+    public string Nombre { get; set; } = "";
+    public string Tels { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Notas { get; set; } = "";
+    public bool Fav { get; set; }
+    public Ctc Clonar() => new() { Id = Id, Nombre = Nombre, Tels = Tels, Email = Email, Notas = Notas, Fav = Fav };
+}
+public sealed class SqliteAlmacenCtc : IDisposable {
+
+    readonly SqliteConnection cn;
+
+    public SqliteAlmacenCtc(string arch) {
+        cn = new(new SqliteConnectionStringBuilder {
+            DataSource = arch
+        }.ConnectionString);
+
+        cn.Open();
+    }
+
+    public void CrearTablas() => cn.Execute("""
+        CREATE TABLE IF NOT EXISTS Contactos(
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Nombre TEXT NOT NULL,
+            Tels TEXT NOT NULL DEFAULT '',
+            Email TEXT NOT NULL DEFAULT '',
+            Notas TEXT NOT NULL DEFAULT '',
+            Fav INTEGER NOT NULL DEFAULT 0
+        );
+    """);
+
+    public IEnumerable<Ctc> ObtenerTodos()
+        => cn.GetAll<Ctc>();
+
+    public Ctc Agregar(Ctc ctc) {
+        ctc.Id = 0;
+        ctc.Id = Convert.ToInt32(cn.Insert(ctc));
+        return ctc;
+    }
+}
