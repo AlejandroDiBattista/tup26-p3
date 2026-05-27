b2dbb879010dc1eec122aa5fe091f1f5b7bb4a06
Franco Tomas Garcia <143847992+FrancoTms@users.noreply.github.com>
2026-05-27 19:26:03 -0300

    Añado modelo de configuración AppConfig y SortField

--- enunciados/tp1/sortx.cs
+++ enunciados/tp1/sortx.cs
@@ -3,5 +3,18 @@
 //       [-i|--input input] [-o|--output output]
 //       [-d|--delimiter delimitador]
 //       [-nh|--no-header] [-h|--help]
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
-Console.WriteLine($"sortx {string.Join(" ", args)}");
+//Modelo de configuración
+record AppConfig(
+    string? InputFile,
+    string? OutputFile,
+    string Delimiter,
+    bool NoHeader,
+    List<SortField> SortFields
+);
+record SortField(string Name, bool Numeric, bool Descending);
