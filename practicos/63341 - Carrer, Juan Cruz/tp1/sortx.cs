c49960b97320809a9054d81bbcc55130940662e9
juancarrer11 <juancarrer11@gmail.com>
2026-05-27 19:36:32 -0300

    Agregar función ParseArgs para procesar argumentos de entrada y configuración

--- practicos/63341 - Carrer, Juan Cruz/tp1/sortx.cs
+++ practicos/63341 - Carrer, Juan Cruz/tp1/sortx.cs
@@ -4,6 +4,69 @@ using   System.IO;
 using   System.Linq;
 using   System.Text;
 
+AppConfig ParseArgs(string[] args)
+{
+    string? inputFile = null;
+    string? outputFile = null;
+    string delimiter = ",";
+    bool noHeader = false;
+    var sortFields = new List<SortField>();
+
+    for (int i = 0; i < args.Length; i++)
+    {
+        var arg = args[i];
+
+        switch (args[i])
+        {
+            case "-i":
+            case"--input":
+                input = args[++i];
+                break;
+
+            case "-o":
+            case "--output":
+                outputFile = args[++i];
+                break;
+
+            case "-d":
+            case "--delimiter":
+                delimiter = args[++i];
+                break;
+
+            case "-nh":
+            case "--no-header":
+                noHeader = true;
+                break;
+
+            case "-h":
+            case "--help":
+                ShowHelp();
+                Environment.Exit(0);
+                break;
+
+            case "-b":
+            case "--by":
+                var spec = args[++i];
+                sortFields.Add(ParseSortField(spec));
+                break;
+
+            default:
+                if (arg.StartsWith("-"))
+                    throw new ArgumentException($"Opción desconocida: {arg}");
+                
+                positionals.Add(arg);
+                break;
+        }
+    }
+
+    if (positionals.Count > 0 && inputFile == null)
+        inputFile = positionals[0];
+
+    if (positionals.Count > 1 && outputFile == null)
+        outputFile = positionals[1];
+
+    return new AppConfig(inputFile, outputFile, delimiter, noHeader, sortFields);
+}
 
 record SortField(string Nombre, bool Numerico, bool Descendiente);
 
