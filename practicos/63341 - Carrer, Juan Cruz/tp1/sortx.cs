2479fe9f60dba7b2a6a897b2834adce6f522bdb9
juancarrer11 <juancarrer11@gmail.com>
2026-05-27 19:21:19 -0300

    Agregar registros SortField y AppConfig para la configuración de ordenamiento

--- practicos/63341 - Carrer, Juan Cruz/tp1/sortx.cs
+++ practicos/63341 - Carrer, Juan Cruz/tp1/sortx.cs
@@ -1,7 +1,16 @@
+using   System;
+using   System.Collections.Generic;
+using   System.IO;
+using   System.Linq;
+using   System.Text;
 
-// sortx [input [output]] [-b|--by campo[:tipo[:orden]]]...
-//       [-i|--input input] [-o|--output output]
-//       [-d|--delimiter delimitador]
-//       [-nh|--no-header] [-h|--help]
 
-Console.WriteLine($"sortx {string.Join(" ", args)}");
+record SortField(string Nombre, bool Numerico, bool Descendiente);
+
+record AppConfig(
+    string? InputFile,
+    string? OutputFile,
+    string Delimiter,
+    bool NoHeader,
+    List<SortField> SortFields
+);
