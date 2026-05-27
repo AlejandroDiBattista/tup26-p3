7b1f994819c7d4d5b6412c1c2d0f2f24ec2eb5d3
Franco Tomas Garcia <143847992+FrancoTms@users.noreply.github.com>
2026-05-27 19:35:08 -0300

    Parseo de argumentos básicos -i y -o

--- enunciados/tp1/sortx.cs
+++ enunciados/tp1/sortx.cs
@@ -9,6 +9,31 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
+// 1. ParseArgs      → leer la configuración desde los argumentos
+AppConfig ParseArgs(string[] args)
+{
+    string? input = null;
+    string? output = null;
+    var positionals = new List<string>();
+    for(int i = 0; i < args.Length; i++)
+    {
+        var arg = args[i];
+        switch (arg)
+        {
+            case "-i":
+            case "--input":
+                input = args[++i];
+                continue;
+            case "-o":
+            case "--output":
+                output = args[++i];
+                continue;
+        }
+    }
+    if(positionals.Count > 0 && input == null) input = positionals[0];
+    if(positionals.Count > 1 && output == null) output = positionals[1];
+    return new AppConfig(input, output);
+}
 //Modelo de configuración
 record AppConfig(
     string? InputFile,
