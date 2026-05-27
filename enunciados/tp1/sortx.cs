bd1ffc066ac689cfb0d231ec31f897e736358ace
Franco Tomas Garcia <143847992+FrancoTms@users.noreply.github.com>
2026-05-27 19:43:25 -0300

    Soporte de flags adicionales -d, -h, -nh y creación de ShowHelp

--- enunciados/tp1/sortx.cs
+++ enunciados/tp1/sortx.cs
@@ -14,6 +14,8 @@ AppConfig ParseArgs(string[] args)
 {
     string? input = null;
     string? output = null;
+    string delimiter = ",";
+    bool noHeader = false;
     var positionals = new List<string>();
     for(int i = 0; i < args.Length; i++)
     {
@@ -28,11 +30,45 @@ AppConfig ParseArgs(string[] args)
             case "--output":
                 output = args[++i];
                 continue;
+            case "-d":
+            case "--delimiter":
+                delimiter = args[++i];
+                continue;
+            case "-nh":
+            case "--no-header":
+                noHeader = true;
+                continue;
+            case "-h":
+            case "--help":
+                showHelp();
+                Environment.Exit(0);
+                continue;
         }
     }
     if(positionals.Count > 0 && input == null) input = positionals[0];
     if(positionals.Count > 1 && output == null) output = positionals[1];
-    return new AppConfig(input, output);
+    return new AppConfig(input, output, delimiter, noHeader);
+}
+
+//ShowHelp
+void ShowHelp()
+{
+    Console.WriteLine(@"
+    Uso:
+        sortx [input [output]] -b campo[:tipo[:orden]]...
+
+    Opciones:
+    -b, --by           Campo de ordenamiento
+    -i, --input        Archivo de entrada
+    -o, --output       Archivo de salida
+    -d, --delimiter    Delimitador (default ,)
+    -nh, --no-header   Sin encabezado
+    -h, --help         Mostrar ayuda
+
+    Ejemplos:
+    sortx empleados.csv -b apellido
+    sortx empleados.csv -b salario:num:desc
+    ");
 }
 //Modelo de configuración
 record AppConfig(
