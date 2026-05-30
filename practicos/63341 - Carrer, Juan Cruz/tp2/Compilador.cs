b2f2db939bcca8169d8c4d40c2f9daae2b2e4a89
juancarrer11 <juancarrer11@gmail.com>
2026-05-30 20:39:03 -0300

    Creo estructura base del parser

--- practicos/63341 - Carrer, Juan Cruz/tp2/Compilador.cs
+++ practicos/63341 - Carrer, Juan Cruz/tp2/Compilador.cs
@@ -1,5 +1,77 @@
 class Compilador {
-    public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+
+    private string codigoFuente = "";
+    private int posicionActual = 0;
+
+    public static Nodo Parse(
+        string expresion
+    ) {
+
+        var parser = new Compilador();
+
+        parser.codigoFuente = expresion;
+        parser.posicionActual = 0;
+
+        return parser.LeerExpresion();
+    }
+
+    private Nodo LeerExpresion() {
+
+        var nodoActual = LeerTermino();
+
+        while (true) {
+
+            if (Coincide('+')) {
+
+                nodoActual =
+                    new OperacionSuma(
+                        nodoActual,
+                        LeerTermino()
+                    );
+            }
+            else if (Coincide('-')) {
+
+                nodoActual =
+                    new OperacionResta(
+                        nodoActual,
+                        LeerTermino()
+                    );
+            }
+            else {
+                break;
+            }
+        }
+
+        return nodoActual;
+    }
+
+    private Nodo LeerTermino() {
+
+        var nodoActual = LeerFactor();
+
+        while (true) {
+
+            if (Coincide('*')) {
+
+                nodoActual =
+                    new OperacionMultiplicacion(
+                        nodoActual,
+                        LeerFactor()
+                    );
+            }
+            else if (Coincide('/')) {
+
+                nodoActual =
+                    new OperacionDivision(
+                        nodoActual,
+                        LeerFactor()
+                    );
+            }
+            else {
+                break;
+            }
+        }
+
+        return nodoActual;
     }
 }
