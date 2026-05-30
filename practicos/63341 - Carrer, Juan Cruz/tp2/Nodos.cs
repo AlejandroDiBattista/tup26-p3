e23f1bfd89a6dc4eb1b3ea79158980129d9dc7cf
juancarrer11 <juancarrer11@gmail.com>
2026-05-30 20:17:16 -0300

    Agrego operaciones binarias

--- practicos/63341 - Carrer, Juan Cruz/tp2/Nodos.cs
+++ practicos/63341 - Carrer, Juan Cruz/tp2/Nodos.cs
@@ -21,3 +21,94 @@ class VariableX : Nodo {
         return x;
     }
 }
+
+class CambioSigno : Nodo {
+
+    private Nodo contenido;
+
+    public CambioSigno(Nodo nodo) {
+        contenido = nodo;
+    }
+
+    public override int Evaluar(int x = 0) {
+        return -contenido.Evaluar(x);
+    }
+}
+
+abstract class OperacionBinaria : Nodo {
+
+    protected Nodo izquierda;
+    protected Nodo derecha;
+
+    public OperacionBinaria(
+        Nodo izq,
+        Nodo der
+    ) {
+
+        izquierda = izq;
+        derecha = der;
+    }
+}
+
+class OperacionSuma : OperacionBinaria {
+
+    public OperacionSuma(
+        Nodo izq,
+        Nodo der
+    ) : base(izq, der) { }
+
+    public override int Evaluar(int x = 0) {
+
+        return izquierda.Evaluar(x)
+            + derecha.Evaluar(x);
+    }
+}
+
+class OperacionResta : OperacionBinaria {
+
+    public OperacionResta(
+        Nodo izq,
+        Nodo der
+    ) : base(izq, der) { }
+
+    public override int Evaluar(int x = 0) {
+
+        return izquierda.Evaluar(x)
+            - derecha.Evaluar(x);
+    }
+}
+
+class OperacionMultiplicacion : OperacionBinaria {
+
+    public OperacionMultiplicacion(
+        Nodo izq,
+        Nodo der
+    ) : base(izq, der) { }
+
+    public override int Evaluar(int x = 0) {
+
+        return izquierda.Evaluar(x)
+            * derecha.Evaluar(x);
+    }
+}
+
+class OperacionDivision : OperacionBinaria {
+
+    public OperacionDivision(
+        Nodo izq,
+        Nodo der
+    ) : base(izq, der) { }
+
+    public override int Evaluar(int x = 0) {
+
+        var divisor =
+            derecha.Evaluar(x);
+
+        if (divisor == 0) {
+            throw new DivideByZeroException();
+        }
+
+        return izquierda.Evaluar(x)
+            / divisor;
+    }
+}
