67d945ac7ff383c583197b4dbe051ad3665520bd
juancarrer11 <juancarrer11@gmail.com>
2026-05-30 19:29:53 -0300

    Agrego nodos numericos y variables

--- practicos/63341 - Carrer, Juan Cruz/tp2/Nodos.cs
+++ practicos/63341 - Carrer, Juan Cruz/tp2/Nodos.cs
@@ -1,3 +1,23 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
+
+class ValorNumero : Nodo {
+
+    private int numeroGuardado;
+
+    public ValorNumero(int valor) {
+        numeroGuardado = valor;
+    }
+
+    public override int Evaluar(int x = 0) {
+        return numeroGuardado;
+    }
+}
+
+class VariableX : Nodo {
+
+    public override int Evaluar(int x = 0) {
+        return x;
+    }
+}
