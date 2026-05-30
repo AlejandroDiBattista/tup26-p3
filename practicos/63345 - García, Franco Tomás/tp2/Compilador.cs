e0bf6b2b7686e1af775b18e38a556f192aaf3b49
Franco Tomas Garcia <143847992+FrancoTms@users.noreply.github.com>
2026-05-30 18:29:24 -0300

    Parser básico

--- practicos/63345 - García, Franco Tomás/tp2/Compilador.cs
+++ practicos/63345 - García, Franco Tomás/tp2/Compilador.cs
@@ -1,5 +1,59 @@
 class Compilador {
+    private string texto = "";
+    private int indice = 0;
+
     public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+        if (string.IsNullOrWhiteSpace(expresion))
+            throw new FormatException("Token inesperado");
+
+        var parser = new Compilador {
+            texto = expresion,
+            indice = 0
+        };
+
+        var nodo = parser.ParseValor();
+
+        parser.SaltarEspacios();
+
+        if (parser.indice < parser.texto.Length)
+            throw new FormatException("Token inesperado");
+
+        return nodo;
+    }
+
+    private Nodo ParseValor() {
+        SaltarEspacios();
+
+        char actual = VerActual();
+
+        if (char.IsDigit(actual))
+            return ParseNumero();
+
+        if (char.ToLower(actual) == 'x') {
+            indice++;
+            return new NodoVariable();
+        }
+
+        throw new FormatException("Token inesperado");
+    }
+
+    private Nodo ParseNumero() {
+        int inicio = indice;
+
+        while (char.IsDigit(VerActual()))
+            indice++;
+
+        string numero = texto[inicio..indice];
+
+        return new NodoNumero(int.Parse(numero));
+    }
+
+    private char VerActual() {
+        return indice < texto.Length ? texto[indice] : '\0';
+    }
+
+    private void SaltarEspacios() {
+        while (char.IsWhiteSpace(VerActual()))
+            indice++;
     }
 }
