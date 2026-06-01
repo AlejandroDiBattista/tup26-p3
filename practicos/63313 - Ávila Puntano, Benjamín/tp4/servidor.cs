aa16333800006fad7906d0bfb1c525ea9416fc53
bnjavila07 <bnjavilapuntano@gmail.com>
2026-05-31 21:11:55 -0300

    inicio modelamiento de datos

--- practicos/63313 - Ávila Puntano, Benjamín/tp4/servidor.cs
+++ practicos/63313 - Ávila Puntano, Benjamín/tp4/servidor.cs
@@ -15,3 +15,44 @@ using (var scope = app.Services.CreateScope())
     var repositorio = scope.ServiceProvider.GetRequiredService<CatalogoRepositorio>();
     repositorio.Iniciar();
 }
+
+app.Run("http://localhost:5000"); // iniciamos el servidor en el puerto 5000
+
+enum TipoMovimiento{Compra,Venta,Ajuste} //Tipo de movimientos que se podran realizar
+
+
+
+// definicion de la clase producto, con sus propiedades Id, Codigo, Nombre, Stock y Precio
+// usamos "" para evitar warnings de null
+class Producto
+{
+    public int Id { get; set; }
+    public string Codigo { get; set; } = null!;
+    public string Nombre { get; set; } = null!;
+    public int Stock { get; set; }
+    public decimal Precio { get; set; }
+}
+
+public List<MovimientoDeProducto> Movimientos { get; set; } = [];
+ // lista de movimientos de productos, se usa =[] para inicar la lista vacia y evitar nullreference exceptions
+ 
+
+ class MovimientoDeProducto {
+    public int Id{get;set;}
+    public int ProductoId{get;set;}
+}
+// clase para los movimientos de productos, con sus propiedades Id y ProductoId, que se relaciona con la clase Producto a traves de ProductoId
+
+
+
+public Producto? Producto { get; set; } // propiedad de tipo Producto que se relaciona con la clase MovimientoDeProducto a traves de ProductoId
+// propiedades para el historial del stock 
+public TipoMovimiento Tipo { get; set; }
+public int Cantidad { get; set; }
+public DateTime Fecha { get; set; }
+
+
+record ProductoEntrada(string Codigo, string Nombre, int Stock, decimal Precio); // record para la entrada de producto
+record MovimientoEntrada(TipoMovimiento Tipo, int Cantidad); // record para la entrada de movimiento de producto
+record OperacionProducto(Producto? Producto = null, string? Error = null, bool NoEncontrado = false);
+record OperacionMovimiento(MovimientoDeProducto? Movimiento = null, string? Error = null, bool NoEncontrado = false);
