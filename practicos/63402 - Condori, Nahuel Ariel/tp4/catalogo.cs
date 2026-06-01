f5581912b1457157581bf107438c1462e5710b30
Nahuel Condori <nahuelariel12@gmail.com>
2026-06-01 04:48:29 -0300

    Paso 1 de catalogo.cs
    declaro una lista de productos, intento conectarme al servidor, creo un httpclient, cargo productos desde /productos y si falla, muestro un msj de error y corto el programa.

--- practicos/63402 - Condori, Nahuel Ariel/tp4/catalogo.cs
+++ practicos/63402 - Condori, Nahuel Ariel/tp4/catalogo.cs
@@ -6,3 +6,15 @@ using System.Net.Http.Json;
 using Terminal.Gui.App;
 using Terminal.Gui.ViewsBase;
 using Terminal.Gui.Views;
+
+
+List <ProductoDto> productos;
+try
+{
+    using var http = new HttpClient();
+    productos = await CargarProductosAsync(http);
+} catch (httpRequestException ex) {
+    Console.WriteLine($"Error al cargar productos: {ex.Message}");
+    Console.WriteLine("Verificá que servidor.cs este corriendo en http://localhost:5050");
+    return;
+}
