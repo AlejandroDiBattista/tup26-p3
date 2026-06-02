af2499d9ea6e06cc73bd86fc9c5231c8cb10df43
Joacoavila9 <joaquinavilalahr330@gmail.com>
2026-06-02 00:25:44 -0300

    consultas por id

--- practicos/63456 - Ávila Lahr, Joaquín/tp4/servidor.cs
+++ practicos/63456 - Ávila Lahr, Joaquín/tp4/servidor.cs
@@ -24,7 +24,38 @@ using (var scope = app.Services.CreateScope()) {
 
 app.MapGet("/productos", (CatalogoRepositorio repositorio) => {
     return Results.Ok(repositorio.TraerProductos());
+    
+    
 });
+app.MapGet("/productos/{id}", (int id, CatalogoRepositorio repositorio) =>
+{
+    var producto = repositorio.TraerProducto(id);
+
+    return producto is null
+        ? Results.NotFound()
+        : Results.Ok(producto);
+});
+
+app.MapPost("/productos", (Producto producto, CatalogoRepositorio repositorio) =>
+{
+    var nuevo = repositorio.AgregarProducto(producto);
+    return Results.Created($"/productos/{nuevo.Id}", nuevo);
+});
+
+app.MapPut("/productos/{id}", (int id, Producto producto, CatalogoRepositorio repositorio) =>
+{
+    return repositorio.ModificarProducto(id, producto)
+        ? Results.NoContent()
+        : Results.NotFound();
+});
+
+app.MapDelete("/productos/{id}", (int id, CatalogoRepositorio repositorio) =>
+{
+    return repositorio.EliminarProducto(id)
+        ? Results.NoContent()
+        : Results.NotFound();
+});
+
 
 app.Run("http://localhost:5050");
 
@@ -55,9 +86,9 @@ class CatalogoRepositorio {
     if (!db.Productos.Any())
     {
         db.Productos.AddRange(
-            new Producto(1, "P001", "Yerba Mate 500g", 1500m, 100),
-            new Producto(2, "P002", "Azúcar 1kg", 1200m, 50),
-            new Producto(3, "P003", "Café 250g", 2500m, 25)
+            new Producto(1, "P001", "Teclado Redragon", 45000m, 100),
+            new Producto(2, "P002", "Mouse Logitech", 30000m, 50),
+            new Producto(3, "P003", "Monito Lg", 100000m, 25)
         );
 
         db.SaveChanges();
@@ -68,4 +99,39 @@ public List<Producto> TraerProductos() =>
     db.Productos
       .OrderBy(p => p.Id)
       .ToList();
+public Producto? TraerProducto(int id) =>
+    db.Productos.Find(id);
+
+public Producto AgregarProducto(Producto producto)
+{
+    db.Productos.Add(producto);
+    db.SaveChanges();
+    return producto;
+}
+
+public bool ModificarProducto(int id, Producto productoActualizado)
+{
+    var producto = db.Productos.Find(id);
+
+    if (producto is null)
+        return false;
+
+    db.Entry(producto).CurrentValues.SetValues(productoActualizado);
+    db.SaveChanges();
+
+    return true;
+}
+
+public bool EliminarProducto(int id)
+{
+    var producto = db.Productos.Find(id);
+
+    if (producto is null)
+        return false;
+
+    db.Productos.Remove(producto);
+    db.SaveChanges();
+
+    return true;
+}
 }
