e56710d0fa7ac86b112158000b626124125cb5e9
tatianaherrera-commits <aatianaherrera350@gmail.com>
2026-06-02 19:17:27 -0300

    Datos

--- practicos/63231 - Herrera, Rocío Tatiana/tp4/servidor.cs
+++ practicos/63231 - Herrera, Rocío Tatiana/tp4/servidor.cs
@@ -13,3 +13,16 @@ builder.Services.ConfigureHttpJsonOptions(opt => {
 builder.Services.AddDbContext<CatalogoDb>(opt => opt.UseSqlite("Data Source=catalogo.db"));
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope()) {
+    var db = scope.ServiceProvider.GetRequiredService<CatalogoDb>();
+    db.Database.EnsureCreated();
+
+    if (!db.Productos.Any()) {
+        db.Productos.AddRange(
+            new Producto { Codigo = "P001", Nombre = "Yerba Mate 500g", Precio = 1500m, Stock = 100 },
+            new Producto { Codigo = "P002", Nombre = "Azucar 1kg", Precio = 900m, Stock = 60 },
+            new Producto { Codigo = "P003", Nombre = "Cafe molido 250g", Precio = 2500m, Stock = 35 }
+        );
+        db.SaveChanges();
+    }
+}
