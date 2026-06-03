0e331dc90050d0f2cffff5baa5e8209499c12c63
tatianaherrera-commits <aatianaherrera350@gmail.com>
2026-06-02 21:18:09 -0300

    cliente

--- practicos/63231 - Herrera, Rocío Tatiana/tp4/catalogo.cs
+++ practicos/63231 - Herrera, Rocío Tatiana/tp4/catalogo.cs
@@ -1,1 +1,14 @@
+#:package Terminal.Gui@2.*
+#:property PublishAot=false
 
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Terminal.Gui.App;
+using Terminal.Gui.ViewBase;
+using Terminal.Gui.Views;
+
+const string ApiBase = "http://localhost:5050";
