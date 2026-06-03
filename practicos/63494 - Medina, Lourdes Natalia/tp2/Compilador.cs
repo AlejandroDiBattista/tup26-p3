022550e77e43899a72a5f43a0c5b8934204ebf84
lumedina23 <lourdesnatalimedina348@gmail.com>
2026-06-03 15:23:32 -0300

    Implementar el método ParseFactor para mejorar el análisis de factores en expresiones.

--- practicos/63494 - Medina, Lourdes Natalia/tp2/Compilador.cs
+++ practicos/63494 - Medina, Lourdes Natalia/tp2/Compilador.cs
@@ -67,6 +67,45 @@ class Compilador{
         }
         return nodo;
     }
+
+     private static Nodo ParseFactor(){
+         SaltarEspacios();
+         if (Actual == '+'){
+            Avanzar();
+            return ParseFactor();
+        }
+        if (Actual == '-'){
+            Avanzar();
+            return new NegativoNodo(ParseFactor());
+        }
+        if (Actual == '('){
+            Avanzar();
+            var nodo = ParseExpresion();
+            SaltarEspacios();
+            if (Actual != ')')
+                throw new FormatException("Se esperaba ')'");
+            Avanzar();
+            return nodo;
+        }
+        if (char.IsDigit(Actual)){
+            int inicio = pos;
+            while (char.IsDigit(Actual))
+                Avanzar();
+            
+            int numero = int.Parse(texto.Substring(inicio, pos - inicio));
+            return new NumeroNodo(numero);
+        }
+
+        if (Actual == 'x' || Actual == 'X')
+        {
+            Avanzar();
+            return new VariableNodo();
+        }
+        throw new FormatException("Token inesperado");
+    }
 }
+
+
+
 
 
