8b5d897286533d23f754431fe1789421bbce0d9c
lumedina23 <lourdesnatalimedina348@gmail.com>
2026-06-03 13:12:58 -0300

    Se crea la clase abstracta Nodo y NumeroNodo

--- practicos/63494 - Medina, Lourdes Natalia/tp2/Nodos.cs
+++ practicos/63494 - Medina, Lourdes Natalia/tp2/Nodos.cs
@@ -1,3 +1,16 @@
-abstract class Nodo {
-    public abstract int Evaluar(int x = 0);
+using System;
+
+abstract class Nodo{
+    public abstract int Evaluar(int x);
+}
+
+class NumeroNodo : Nodo{
+    private int valor;
+
+    public NumeroNodo(int valor) {
+        this.valor = valor;
+    }
+
+    public override int Evaluar(int x) => valor;
+   
 }
