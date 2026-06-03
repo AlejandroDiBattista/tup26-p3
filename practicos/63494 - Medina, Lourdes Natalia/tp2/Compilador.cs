c9c16a451d72f0ea06ba1ee2968de857260e832a
lumedina23 <lourdesnatalimedina348@gmail.com>
2026-06-03 13:55:38 -0300

    Implementar la clase Compilador y el método Parse para procesar expresiones

---
+++ practicos/63494 - Medina, Lourdes Natalia/tp2/Compilador.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Compilador{
+    private static string texto = "";
+    private static int pos;
+
+    public static Nodo Parse(string expresion){
+        if (string.IsNullOrWhiteSpace(expresion))
+        throw new FormatException("Token inesperado");
+
+            texto = expresion;
+            pos = 0;
+
+            var nodo = ParseExpresion();
+
+        SaltarEspacios();
+
+        if (pos < texto.Length)
+        throw new FormatException("Token inesperado");
+        return nodo;
+    }
+
+    private static char Actual => pos < texto.Length ? texto[pos] : '\0';
+    private static void Avanzar() => pos++;
+    private static void SaltarEspacios(){
+      while(char.IsWhiteSpace(Actual)) Avanzar(); 
+    }
+}
