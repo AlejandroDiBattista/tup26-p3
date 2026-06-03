2df673f76a398a19cdb63a2b6c8854829298c302
valepaz11 <vpaz27985@gmail.com>
2026-06-02 23:42:06 -0300

    Update catalogo.cs

--- practicos/63647 - Paz, Valentina/tp4/catalogo.cs
+++ practicos/63647 - Paz, Valentina/tp4/catalogo.cs
@@ -5,6 +5,7 @@ using System.Net.Http.Json;
 using Terminal.Gui;
 using Terminal.Gui.App;
 using Terminal.Gui.Views;
+using Terminal.Gui.Input;
 
 
 // ── Consulta inicial al servidor ──────────────────────────────────────────
@@ -24,29 +25,44 @@ catch (HttpRequestException ex) {
 
 using IApplication app = Application.Create().Init();
 using Window ventana = new () { Title = " Catalogo REST — Producto (ESC para salir) " };
+int productoSeleccionado = 2;
 
 var lista = new ListView() {
     X = 1,
     Y = 2,
-    Width = 30,
+    Width = 55,
     Height = 20
 };
 var detalle = new Label() {
-    X = 42,
-    Y = 2,
-    Width = 30,
-    Height = 10,
+    X = 60,
+    Y = 9,
+    Width = 50,
+    Height = 15,
     Text = "Seleccione un producto"
+};
+var infoProducto = new Label() {
+    X = 60,
+    Y = 2,
+    Width = 50,
+    Height = 6,
+    Text = ""
 };
 
 lista.SetSource(
     new System.Collections.ObjectModel.ObservableCollection<string>
-       ( productos.Select(p => $"{p.Codigo} - {p.Nombre}" ). ToList()
+       ( productos.Select(p => $"{p.Codigo} | {p.Nombre} | ${p.Precio} | Stock: {p.Stock}" ). ToList()
     )
 );
 
 if (productos.Count > 0)
 {
+
+     infoProducto.Text =
+     $"Código: {producto.Codigo}\n" +
+     $"Nombre: {producto.Nombre}\n" +
+     $"Precio: ${producto.Precio:N0}\n" +
+     $"Stock: {producto.Stock}";
+
     using var http = new HttpClient();
     var movimientos = await CargarMovimientosAsync(http, 2);
 
@@ -57,7 +73,7 @@ if (productos.Count > 0)
     }
     else {
         detalle.Text = string.Join("\n", 
-        movimientos.Select(m => $"{m.Tipo} | Cant: {m.Cantidad} "));
+        movimientos.Select(m => $"{m.Tipo} | Cant: {m.Cantidad} | Fecha: {m.Fecha:g}"));
     }
 }
  
@@ -68,15 +84,151 @@ var tituloProductos = new Label() {
 };
 
 var tituloDetalle = new Label() {
-     X = 42,
+     X = 60,
      Y = 0,
-     Text = "Movimientos"
+     Text = " Detalles y Movimientos"
 };
 
 ventana.Add(tituloProductos);
 ventana.Add(tituloDetalle);
 ventana.Add(lista);
+lista.Accepting += async (_, _) =>
+{
+    productoSeleccionado = productos[lista.SelectedItem ?? 0 ].Id
+
+    var producto = productos[lista.SelectedItem ?? 0];
+
+    infoProducto.Text =
+        $"Código: {producto.Codigo}\n" +
+        $"Nombre: {producto.Nombre}\n" +
+        $"Precio: ${producto.Precio:N0}\n" +
+        $"Stock: {producto.Stock}";
+
+    using var http = new HttpClient();
+
+    var movimientos = await CargarMovimientosAsync(http, productoSeleccionado);
+
+    if (movimientos.Count == 0)
+    {
+        detalle.Text = "Sin movimientos registrados";
+    }
+    else
+    {
+        detalle.Text = string.Join(
+            "\n",
+            movimientos.Select(m =>
+                $"{m.Tipo} | Cant: {m.Cantidad} | Fecha: {m.Fecha:g}")
+        );
+    }
+};
+ventana.Add(infoProducto);
 ventana.Add(detalle);
+
+var botonCompra = new Button()
+{
+    X = 60,
+    Y = 18,
+    Text = "_Compra +10"
+};
+ventana.Add(botonCompra);
+botonCompra.SetFocus();
+
+var botonVenta = new Button()
+{
+    X = 60,
+    Y = 20,
+    Text = "_Venta -5"
+};
+
+ventana.Add(botonVenta);
+
+botonVenta.Accepting += async (_, _) =>
+{
+    using var http = new HttpClient();
+
+    await http.PostAsync(
+        $"http://localhost:5050/productos/{productoSeleccionado}/movimientos?tipo=Venta&cantidad=5",
+        null
+    );
+};
+
+botonCompra.Accepting += async (_, _) =>
+{
+    using var http = new HttpClient();
+
+    await http.PostAsync(
+        $"http://localhost:5050/productos/{productoSeleccionado}/movimientos?tipo=Compra&cantidad=10",
+        null
+    );
+};
+
+var botonAjuste = new Button()
+{
+    X = 60,
+    Y = 22,
+    Text = "_Ajuste 100"
+};
+
+ventana.Add(botonAjuste);
+
+botonAjuste.Accepting += async (_, _) =>
+{
+    using var http = new HttpClient();
+
+    await http.PostAsync(
+        $"http://localhost:5050/productos/{productoSeleccionado}/movimientos?tipo=Ajuste&cantidad=100",
+        null
+    );
+};
+
+var ayuda = new Label() {
+    X = 1,
+    Y = 22,
+    Text = "Use Enter para seleccionar un producto. Alt+C Compra | Alt+V Venta | Alt+A Ajuste"
+};
+
+ventana.Add(ayuda);
+
+var botonAgregar = new Button()
+{
+    X = 60,
+    Y = 24,
+    Text = "_Agregar"
+};
+
+var botonModificar = new Button()
+{
+    X = 75,
+    Y = 24,
+    Text = "_Modificar"
+};
+
+var botonEliminar = new Button()
+{
+    X = 95,
+    Y = 24,
+    Text = "_Eliminar"
+};
+
+ventana.Add(botonAgregar);
+ventana.Add(botonModificar);
+ventana.Add(botonEliminar);
+
+botonAgregar.Accepting += (_, _) =>
+{
+    Console.WriteLine("Agregar producto");
+};
+
+botonModificar.Accepting += (_, _) =>
+{
+    Console.WriteLine("Modificar producto");
+};
+
+botonEliminar.Accepting += (_, _) =>
+{
+    Console.WriteLine("Eliminar producto");
+};
+
 
 app.Run(ventana);
 
@@ -101,7 +253,7 @@ static async Task<List<MovimientoDto>> CargarMovimientosAsync(HttpClient http, i
 record ProductoDto(int Id, string Codigo, string Nombre, decimal Precio, int Stock);
 
 record MovimientoDto(
-    int Id, int productoId, TipoMovimiento Tipo, int Cantidad
+    int Id, int productoId, TipoMovimiento Tipo, int Cantidad, DateTime Fecha 
 );
 
 enum TipoMovimiento{
