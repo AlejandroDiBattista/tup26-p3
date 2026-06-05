39c47de1fad01169a645df101f3786464973dfbe
GuilleCortes01 <guillecortes100@gmail.com>
2026-06-05 16:36:45 -0300

    Agregue nodos binarios de multiplicacion y division

--- practicos/63218 - Cortés, Guillermo Augusto/tp2/Nodos.cs
+++ practicos/63218 - Cortés, Guillermo Augusto/tp2/Nodos.cs
@@ -91,3 +91,29 @@ class RestaNodo : NodoBinario
         return izquierda.Evaluar(x) - derecha.Evaluar(x);
     }
 }
+
+class MultiplicacionNodo : NodoBinario
+{
+    public MultiplicacionNodo(Nodo izquierda, Nodo derecha) : base(izquierda, derecha)
+    {
+        
+    }
+
+    public override int Evaluar(int x = 0)
+    {
+        return izquierda.Evaluar(x) * derecha.Evaluar(x);
+    }
+}
+
+class DivisionNodo : NodoBinario
+{
+    public DivisionNodo(Nodo izquierda, Nodo derecha) : base(izquierda, derecha)
+    {
+        
+    }
+
+    public override int Evaluar(int x = 0)
+    {
+        return izquierda.Evaluar(x) / derecha.Evaluar(x);
+    }
+}
