fe0c59c5a8ffa5cfd550c2931eef4ee750da6514
Nahuel Almaraz Sintora <nahuelalmaraz957@gmail.com>
2026-06-12 19:02:33 -0300

    agrego verificación de base de datos al iniciar la app

--- practicos/63385 - Almaraz Sintora, Nahuel/tp5/Program.cs
+++ practicos/63385 - Almaraz Sintora, Nahuel/tp5/Program.cs
@@ -1,11 +1,25 @@
 using tp5.Components;
+using Microsoft.EntityFrameworkCore;
+using tp5.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+    builder.Services.AddDbContext<AgendaDbContext>(options =>
+    options.UseSqlite("Data Source=contactos.db"));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AgendaDbContext>();
+    db.Database.EnsureCreated();
+
+    var contactos = db.Contactos.Count();
+    Console.WriteLine($"Base lista. Contactos: {contactos}");
+}
 
 app.UseHttpsRedirection();
 app.UseAntiforgery();
