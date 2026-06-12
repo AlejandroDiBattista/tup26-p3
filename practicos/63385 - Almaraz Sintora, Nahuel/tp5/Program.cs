3eca7ad35a514e610244b74ac4c6701ec384d18b
Nahuel Almaraz Sintora <nahuelalmaraz957@gmail.com>
2026-06-12 20:13:31 -0300

    hago correcciones y configuro la app con base de datos SQLite y middleware básico

--- practicos/63385 - Almaraz Sintora, Nahuel/tp5/Program.cs
+++ practicos/63385 - Almaraz Sintora, Nahuel/tp5/Program.cs
@@ -1,31 +1,22 @@
 using tp5.Components;
-using Microsoft.EntityFrameworkCore;
 using tp5.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-    builder.Services.AddDbContext<AgendaDbContext>(options =>
+builder.Services.AddDbContext<AgendaDbContext>(options =>
     options.UseSqlite("Data Source=contactos.db"));
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AgendaDbContext>();
-    db.Database.EnsureCreated();
-
-    var contactos = db.Contactos.Count();
-    Console.WriteLine($"Base lista. Contactos: {contactos}");
-}
-
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
+    
 app.Run();
