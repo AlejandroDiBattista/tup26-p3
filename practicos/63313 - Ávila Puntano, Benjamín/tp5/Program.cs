29dd3615c9a66980688f7e1bf8c54f80a49281e4
bnjavila07 <bnjavilapuntano@gmail.com>
2026-06-13 09:19:21 -0300

    agregado de agenda db context al program cs

--- practicos/63313 - Ávila Puntano, Benjamín/tp5/Program.cs
+++ practicos/63313 - Ávila Puntano, Benjamín/tp5/Program.cs
@@ -1,15 +1,18 @@
 using tp5.Components;
+using Microsoft.entityframeworkcore;
+using tp5.models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddDbContext<AgendaDbContext>(FileOptions => FileOptions.UseSqLite("DataSource=contactos.db"));
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
