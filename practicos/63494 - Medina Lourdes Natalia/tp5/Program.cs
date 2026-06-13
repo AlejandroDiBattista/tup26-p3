bca2bd4652a77db0518e623a5e1a6f9e9e05207a
lumedina23 <lourdesnatalimedina348@gmail.com>
2026-06-13 20:21:42 -0300

    Reorganizar la configuración de servicios en Program.cs para mejorar la estructura del código

--- practicos/63494 - Medina Lourdes Natalia/tp5/Program.cs
+++ practicos/63494 - Medina Lourdes Natalia/tp5/Program.cs
@@ -13,25 +13,3 @@ string databasePath = Path.Combine(builder.Environment.ContentRootPath, "contact
 builder.Services.AddDbContextFactory<AgendaDbContext>(options =>
     options.UseSqlite($"Data Source={databasePath}"));
 builder.Services.AddScoped<ContactoService>();
-
-var app = builder.Build();
-
-await AgendaSeedData.InitializeAsync(app.Services);
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
