475b90a5dd7a10fda40cf97f11e61ad4d765d72a
Juli Galvan <jrgalvan1996@gmail.com>
2026-06-13 13:59:29 -0300

    agrego dbcontext y conecto  con contcatos.db

--- practicos/63737 - Galván, Rocío Julieta/tp5/Program.cs
+++ practicos/63737 - Galván, Rocío Julieta/tp5/Program.cs
@@ -1,16 +1,21 @@
+using Microsoft.EntityFrameworkCore;
 using tp5.Components;
+using tp5.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddDbContext<AgendaDbContext>(options =>
+    options.UseSqlite("Data Source=contactos.db"));
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
