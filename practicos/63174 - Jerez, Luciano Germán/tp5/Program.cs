33ca920f42fddc82f606cc003d285de4f07d27f0
luchex1525 <luchogoood1894@gmail.com>
2026-06-13 13:29:01 -0300

    Add files via upload

--- practicos/63174 - Jerez, Luciano Germán/tp5/Program.cs
+++ practicos/63174 - Jerez, Luciano Germán/tp5/Program.cs
@@ -1,13 +1,27 @@
-using tp5.Components;
+using Tp5_AgendaWeb.Components;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+ builder.Services.AddDbContextFactory<AgendaWeb.Data.AgendaDbContext>(options =>
+    options.UseSqlite("Data Source=contactos.db"));
+
+builder.Services.AddScoped<AgendaWeb.Services.ContactoService>();    
 
 var app = builder.Build();
 
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
+
 app.UseAntiforgery();
 
 app.MapStaticAssets();
