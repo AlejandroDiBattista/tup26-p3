2c1532764f1e21b200a5b105dea95b5d8c0bc64a
hernancarpalvarez28 <hernancarpalvarez@gmail.com>
2026-06-13 17:46:12 -0300

    complete Program.cs para configurar inyeccion de dependencias y Entity Framework Core.

--- practicos/63300 - Álvarez, Hernán Fabricio/tp5/Program.cs
+++ practicos/63300 - Álvarez, Hernán Fabricio/tp5/Program.cs
@@ -1,13 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using AgendaWeb.Data;
+
 using tp5.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+/*Agregar servicios para blazor*/
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+/*Registrar el DBContext con SQLite */
+builder.Services.AddDbContext<AgendaContext>(options =>
+    options.UseSqlite("Data Source=agenda.db"));
+
+
+
 var app = builder.Build();
 
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
