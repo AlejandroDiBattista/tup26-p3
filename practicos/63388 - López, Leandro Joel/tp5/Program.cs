8c4ab8e7fac4b3a71c433189634789294f226ac5
Leandro Lopez <djlean04052006@gmail.com>
2026-06-12 21:01:19 -0300

    Inicio de proyecto y configuracion EF Core, DbContext y servicio de contactos

--- practicos/63388 - López, Leandro Joel/tp5/Program.cs
+++ practicos/63388 - López, Leandro Joel/tp5/Program.cs
@@ -1,11 +1,35 @@
 using tp5.Components;
+using tp5.Data;
+using tp5.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("AgendaDb") ?? "Data Source=agenda.db"));
+
+    builder.Services.AddScoped<IContactService, ContactService>();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbFactory = scope.ServiceProvider.GetRequiredService<IAppDbContextFactory<AgendaDbContext>>();
+    await using var db = await dbFactory.CreateDbContextAsync();
+    await db.Database.EnsureCreatedAsync();
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    
+    
+}
+
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 app.UseAntiforgery();
