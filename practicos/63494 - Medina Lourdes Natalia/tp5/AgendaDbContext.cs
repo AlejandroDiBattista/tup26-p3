7167c5aed2da1f04a8fffda04cd35e27715401b9
lumedina23 <lourdesnatalimedina348@gmail.com>
2026-06-13 20:55:43 -0300

    Eliminar línea en blanco innecesaria en AgendaDbContext.cs

--- practicos/63494 - Medina Lourdes Natalia/tp5/AgendaDbContext.cs
+++ practicos/63494 - Medina Lourdes Natalia/tp5/AgendaDbContext.cs
@@ -2,7 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using tp5.Models;
 
 namespace tp5.Data;
-
 public class AgendaDbContext(DbContextOptions<AgendaDbContext> options) : DbContext(options)
 {
     public DbSet<Contacto> Contactos => Set<Contacto>();
