a4a39a4b4bd5d271aea9299de82a8e75db386d5b
Facundo Morán <facumamoran@gmail.com>
2026-06-12 21:01:55 -0300

    Registro contexto de datos y conexión SQLite

--- practicos/63412 - Morán, Facundo Martín/tp5/Program.cs
+++ practicos/63412 - Morán, Facundo Martín/tp5/Program.cs
@@ -1,9 +1,17 @@
 using tp5.Components;
+using tp5.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AgendaDbContext>(config =>
+{
+    config.UseSqlite("Data Source=contactos.db");
+});
 
 var app = builder.Build();
 
