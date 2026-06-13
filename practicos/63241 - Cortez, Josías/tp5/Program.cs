4380a594444e5c50acc12814a3ded9d9d976b359
josias57455 <231141799+josias57455@users.noreply.github.com>
2026-06-13 14:57:07 -0300

    Implementar AgendaWeb con CRUD de contactos

--- practicos/63241 - Cortez, Josías/tp5/Program.cs
+++ practicos/63241 - Cortez, Josías/tp5/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using tp5.Components;
+using tp5.Data;
+using tp5.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var dbPath = Path.Combine(builder.Environment.ContentRootPath, "contactos.db");
+builder.Services.AddDbContextFactory<AgendaDbContext>(options =>
+    options.UseSqlite($"Data Source={dbPath}"));
+builder.Services.AddScoped<ContactoService>();
 
 var app = builder.Build();
 
