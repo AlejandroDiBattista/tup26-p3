48b2a17cb3a331cd7d16d0f1165abcf3f73b03de
Karina Edith Condori <kariiedith9315@gmail.com>
2026-06-14 18:37:33 -0300

    Configurar EF Core e inyeccion de dependencias

--- practicos/63268 - Condori, Karina Edith/tp5/Program.cs
+++ practicos/63268 - Condori, Karina Edith/tp5/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using tp5.Components;
+using tp5.Data;
+using tp5.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+string connectionString = builder.Configuration.GetConnectionString("Agenda")
+    ?? "Data Source=contactos.db";
+builder.Services.AddDbContextFactory<AgendaDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddScoped<ContactoService>();
 
 var app = builder.Build();
 
