bb563063e1f38cde4b45c8881235120c317ed390
Luciano Perondi <lucianoperondi90@gmail.com>
2026-06-15 22:42:38 -0300

    acceso a datos a contactos

--- practicos/63354 - Perondi, Luciano/tp5/Program.cs
+++ practicos/63354 - Perondi, Luciano/tp5/Program.cs
@@ -1,9 +1,14 @@
 using tp5.Components;
+using tp5.Datos;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+    builder.Services.AddDbContext<AgendaDb>(opt =>
+    opt.UseSqlite("Data Source=contactos.db"));
 
 var app = builder.Build();
 
