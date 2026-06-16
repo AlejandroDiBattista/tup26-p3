6b6f9334a49513e84c5fe598fcd7a5e427a2cfe5
Joaquin carlino <joacocarlino@gmail.com>
2026-06-16 17:00:35 -0300

    TP5

--- practicos/63150 - Carlino, Joaquín/tp5/Program.cs
+++ practicos/63150 - Carlino, Joaquín/tp5/Program.cs
@@ -1,9 +1,19 @@
 using tp5.Components;
+using tp5.Datos;
+using tp5.Servicios;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// EF Core + SQLite usando una FABRICA de DbContext.
+builder.Services.AddDbContextFactory<AgendaDbContext>(opciones =>
+    opciones.UseSqlite("Data Source=contactos.db"));
+
+// Nuestra logica de aplicacion.
+builder.Services.AddScoped<IContactoService, ContactoService>();
 
 var app = builder.Build();
 
