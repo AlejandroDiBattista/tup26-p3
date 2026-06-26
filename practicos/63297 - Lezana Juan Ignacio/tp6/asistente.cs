54807bd94b346d67f6e5bcb737f5f5d2740284ba
IgnacioLezana25 <nacholezana09@gmail.com>
2026-06-25 22:58:06 -0300

    estructura del proyecto e inicializo con paquetes

--- practicos/63297 - Lezana Juan Ignacio/tp6/asistente.cs
+++ practicos/63297 - Lezana Juan Ignacio/tp6/asistente.cs
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S dotnet run
 #:package DotNetEnv@*
+#:package Microsoft.Extensions.AI@10.4.0
 #:package Microsoft.Extensions.AI.OpenAI@10.4.0
 #:package Terminal.Gui@2.4.3
 #:property PublishAot=false
