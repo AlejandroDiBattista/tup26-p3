10135add6d8c6daabe62c85de7ee6bd2332c2b4a
Atalia Gil Garau <ataliaggcol@gmail.com>
2026-07-02 16:39:46 -0300

    Corrección  tp6

---
+++ practicos/63396 - Gil Garau Atalia Trinidad/tp6/prueba.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero1 = 10;
+        int numero2 = 5;
+        int suma = numero1 + numero2;
+        
+        Console.WriteLine($"La suma de {numero1} y {numero2} es: {suma}");
+    }
+}
